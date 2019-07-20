8ab264c22a029f0ad9f746bb2ce3110c2f0eb649
Michael B. Syson <masarapmabuhay@gmail.com>
2019-07-20 13:35:21 +0800

    +updated: instructions to automatically identify and classify the last name and first name of the member by using the comma, i.e. ",", keyword --> This is when the computer server automatically registers the member after receiving a submitted a report. --> There may, however, be less members calling and/or writing tech support to resolve issues if the report already has two (2) spaces, each for the member's last name and the first name. --> This is instead of only one (1) space for both the last name and the first name. +updated: instructions to automatically verify if the member's name already exists in the database repository --> If the member's name already exists, I've written instructions so that the computer server need not register the member anymore. --> work-in-progress

--- usbong_kms/application/models/Account_Model.php
+++ usbong_kms/application/models/Account_Model.php
@@ -6,29 +6,58 @@ class Account_Model extends CI_Model
 		date_default_timezone_set('Asia/Hong_Kong');
 		$loggedInDateTimeStamp = (new DateTime())->format('Y-m-d H:i:s'); //date('Y-m-d H:i:s');				
 				
-		//TO-DO: -update: patient name to properly be inserted in the last name and first name columns of the member table
 		//TO-DO: -update: member type		
 		//TO-DO: -update: default password
-		$data = array(
-					'member_first_name' => $param['memberNameParam'],
-					'member_type' => 2,
-					'member_password' => password_hash($param['memberNameParam'], PASSWORD_DEFAULT),
+
+		//added by Mike, 20190720		
+		$row = $this->doesMemberAccountExistViaPatientName($param);
+
+		if ($row !== null) {
+			return $row->member_id;
+		}
+		else {					
+			//added by Mike, 20190720
+			$memberNameArray = explode(',', $param['memberNameParam']);
+			$lastName = trim($memberNameArray[0]);
+			$firstName = trim($memberNameArray[1]);
+			
+			$data = array(
+						'member_last_name' => $lastName,
+						'member_first_name' => $firstName,
+						'member_type' => 2,
+						'member_password' => password_hash($param['memberNameParam'], PASSWORD_DEFAULT),
+						'last_logged_in_datetime_stamp' => $loggedInDateTimeStamp								
+					);
+			
+			$this->db->insert('member', $data);
+	/*		
+			$data = array(
+					'customer_first_name' => $param['memberNameParam'],
+					'customer_last_name' => $param['lastNameParam'],
+					'customer_email_address' => $param['emailAddressParam'],
+					'customer_password' => password_hash($param['passwordParam'], PASSWORD_DEFAULT),
 					'last_logged_in_datetime_stamp' => $loggedInDateTimeStamp								
-				);
+			);
+					
+			$this->db->insert('customer', $data);
+	*/		
+			return $this->db->insert_id();
+		}
+	}
+	
+	//added by Mike, 20190720
+	public function doesMemberAccountExistViaPatientName($param) 
+	{
+		$memberNameArray = explode(',', $param['memberNameParam']);
+		$lastName = trim($memberNameArray[0]);
+		$firstName = trim($memberNameArray[1]);
 		
-		$this->db->insert('member', $data);
-/*		
-		$data = array(
-				'customer_first_name' => $param['memberNameParam'],
-				'customer_last_name' => $param['lastNameParam'],
-				'customer_email_address' => $param['emailAddressParam'],
-				'customer_password' => password_hash($param['passwordParam'], PASSWORD_DEFAULT),
-				'last_logged_in_datetime_stamp' => $loggedInDateTimeStamp								
-		);
-				
-		$this->db->insert('customer', $data);
-*/		
-		return $this->db->insert_id();
+		$this->db->select('member_id');
+		$this->db->where('member_last_name', $lastName);
+		$this->db->where('member_first_name', $firstName);
+		$query = $this->db->get('member');
+		$row = $query->row();
+		return $row;
 	}
 }
 ?>
