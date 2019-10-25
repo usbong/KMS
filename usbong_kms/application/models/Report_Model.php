05b7e4e882027e674635de7d4f89d1f0b27836b6
Masarap Mabuhay <masarapmabuhay@gmail.com>
2019-10-25 13:50:08 +0800

    Update Report_Model.php

--- usbong_kms/application/models/Report_Model.php
+++ usbong_kms/application/models/Report_Model.php
@@ -6,14 +6,22 @@ class Report_Model extends CI_Model
 		date_default_timezone_set('Asia/Hong_Kong');
 		$addedDateTimeStamp = (new DateTime())->format('Y-m-d H:i:s'); //date('Y-m-d H:i:s');
 
-		//added by Mike, 20190722
-		$row = $this->doesReportTypeExistViaReportTypeName($param);
-		
+		//added by Mike, 20190722; edited by Mike, 20191025
+//		$row = $this->doesReportTypeExistViaReportTypeName($param);
+//		$row = $this->getReportTypeExistViaReportTypeName($param);
+		$reportTypeId = $this->getReportTypeIdViaReportTypeName($param);
+
+		if ($reportTypeId == False) {			
+			return False; //edited by Mike, 20190722
+		}
+
+/*		
 		if ($row == null) {			
 			return False; //edited by Mike, 20190722
 		}
 		
 		$reportTypeId = $row->report_type_id;
+*/
 
 		$data = array(
 					'member_id' => $param['memberId'],
@@ -29,8 +37,72 @@ class Report_Model extends CI_Model
 		return $this->db->insert_id();		
 	}	
 	
-	//added by Mike, 20190720
-	public function doesReportTypeExistViaReportTypeName($param) 
+	//added by Mike, 20191025
+	public function insertReportsFromAllLocations($param)
+	{			
+		date_default_timezone_set('Asia/Hong_Kong');
+		$addedDateTimeStamp = (new DateTime())->format('Y-m-d H:i:s'); //date('Y-m-d H:i:s');
+
+		//added by Mike, 20190722; edited by Mike, 20191025
+//		$row = $this->doesReportTypeExistViaReportTypeName($param);
+//		$row = $this->getReportTypeExistViaReportTypeName($param);
+		$reportTypeId = $this->getReportTypeIdViaReportTypeName($param);
+
+		if ($reportTypeId == False) {			
+			return False; //edited by Mike, 20190722
+		}
+
+/*		
+		if ($row == null) {			
+			return False; //edited by Mike, 20190722
+		}
+		
+		$reportTypeId = $row->report_type_id;
+*/
+		$data = array(
+					'member_id' => $param['memberId'],
+//					'report_type_id' => $param['reportTypeId'],
+					'report_type_id' => $reportTypeId,
+					'report_item_id' => $param['reportItemId'],
+					'report_answer' => $param['reportAnswerParam'],
+					'added_datetime_stamp' => $addedDateTimeStamp
+				);
+		
+		$this->db->insert('report', $data);
+		
+		return $this->db->insert_id();		
+	}	
+
+	//added by Mike, 20191025
+	public function insertReportFromEachLocation($param)
+	{			
+		date_default_timezone_set('Asia/Hong_Kong');
+		$addedDateTimeStamp = (new DateTime())->format('Y-m-d H:i:s'); //date('Y-m-d H:i:s');
+
+		//added by Mike, 20190722; edited by Mike, 20191025
+		$reportTypeId = $this->getReportTypeIdViaReportTypeName($param);
+
+		if ($reportTypeId == False) {			
+			return False; //edited by Mike, 20190722
+		}
+
+		$data = array(
+					'member_id' => $param['memberId'],
+//					'report_type_id' => $param['reportTypeId'],
+					'report_type_id' => $reportTypeId,
+					'report_item_id' => $param['reportItemId'],
+					'report_answer' => $param['reportAnswerParam'],
+					'added_datetime_stamp' => $addedDateTimeStamp
+				);
+		
+		$this->db->insert('report', $data);
+		
+		return $this->db->insert_id();		
+	}	
+	
+	//added by Mike, 20190720; edited by Mike, 20191025
+//	public function doesReportTypeExistViaReportTypeName($param) 
+	public function getReportTypeIdViaReportTypeName($param) 
 	{		
 //		echo "reportTypeNameParam: ".$param['reportTypeNameParam'];
 	
@@ -38,7 +110,14 @@ class Report_Model extends CI_Model
 		$this->db->where('report_type_name', $param['reportTypeNameParam']);
 		$query = $this->db->get('report_type');
 		$row = $query->row();
-		return $row;
-	}
+		
+		if ($row == null) {			
+			return False; //edited by Mike, 20190722
+		}
+		
+		//$reportTypeId = $row->report_type_id;
+		//return $row;
+		return $row->report_type_id;
+	}	
 }
 ?>
