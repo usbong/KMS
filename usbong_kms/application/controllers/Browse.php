c3e73a46ba42880543426c6a562a85c8287af630
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-03-06 14:24:36 +0800

    Add files via upload

---
+++ usbong_kms/application/controllers/Browse.php
@@ -0,0 +1,123 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Browse extends CI_Controller { //MY_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+//	public function search()//$param)
+	public function index()
+	{
+		$data['param'] = $this->input->get('param'); //added by Mike, 20170616
+
+/*
+		//from application/core/MY_Controller
+		$this::initStyle();
+		$this::initHeaderWith($data);
+		//--------------------------------------------
+		$this->load->view('templates/right_side_bar');
+		//--------------------------------------------
+*/
+
+/*		
+		$this->load->model('Search_Model');
+		$data['result'] = $this->Search_Model->getSearchResult($this->input->get('param'));//$param);
+
+		//added by Mike, 20170825
+		$customer_id = $this->session->userdata('customer_id');
+
+		if ($customer_id==null) {
+			$customer_id=-1;
+		}
+*/
+		
+		date_default_timezone_set('Asia/Hong_Kong');
+		$dateTimeStamp = date('Y/m/d H:i:s');
+	
+/*	
+		$searchData = array(
+				'customer_id' => $customer_id,
+				'searched_item' => $this->input->get('param'),
+				'added_datetime_stamp' => $dateTimeStamp
+		);
+		
+		$this->Search_Model->addSearchedField($searchData);
+*/		
+		
+		$this->load->view('browse', $data);
+
+/*		
+		//--------------------------------------------
+		$this->load->view('templates/footer');	
+*/		
+	}
+	
+	public function confirm()
+	{
+		$data['nameParam'] = $_POST["nameParam"]; //added by Mike, 20170616
+
+/*
+		//from application/core/MY_Controller
+		$this::initStyle();
+		$this::initHeaderWith($data);
+		//--------------------------------------------
+		$this->load->view('templates/right_side_bar');
+		//--------------------------------------------
+*/
+
+/*		
+		$this->load->model('Search_Model');
+		$data['result'] = $this->Search_Model->getSearchResult($this->input->get('param'));//$param);
+
+		//added by Mike, 20170825
+		$customer_id = $this->session->userdata('customer_id');
+
+		if ($customer_id==null) {
+			$customer_id=-1;
+		}
+*/
+		
+		date_default_timezone_set('Asia/Hong_Kong');
+		$dateTimeStamp = date('Y/m/d H:i:s');
+	
+/*	
+		$searchData = array(
+				'customer_id' => $customer_id,
+				'searched_item' => $this->input->get('param'),
+				'added_datetime_stamp' => $dateTimeStamp
+		);
+*/	
+//		$this->Browse_Model->getNamesListViaName($searchData);
+
+
+		$this->load->model('Browse_Model');
+
+		$searchData = array(
+				'searched_item' => $data['nameParam']
+		);
+
+		$data = $this->Browse_Model->getNamesListViaName($searchData);
+
+		echo $data;
+		
+		$this->load->view('browse', $data);
+
+/*		
+		//--------------------------------------------
+		$this->load->view('templates/footer');	
+*/		
+	}
+}
