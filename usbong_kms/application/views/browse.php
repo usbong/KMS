6b1212fdc1435eb02b43a45cf7671b54ba7945d1
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-03-06 14:24:04 +0800

    Add files via upload

---
+++ usbong_kms/application/views/browse.php
@@ -0,0 +1,142 @@
+<!--
+' Copyright 2020 Usbong Social Systems, Inc.
+'
+' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+'
+' http://www.apache.org/licenses/LICENSE-2.0
+'
+' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+'
+' @author: Michael Syson
+' @date created: 20200306
+' @date updated: 20200306
+-->
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset="utf-8">
+
+    <!-- Reference: Apache Friends Dashboard index.html -->
+    <!-- "Always force latest IE rendering engine or request Chrome Frame" -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+    <style type="text/css">
+	/**/
+	                    body
+                        {
+                                font-family: Arial;
+								font-size: 11pt
+                        }
+						
+						div.checkBox
+						{
+								border: 1.5pt solid black; height: 9pt; width: 9pt;
+								text-align: center;
+								float: left
+						}
+						
+						div.option
+						{
+								padding: 2pt;
+								display: inline-block;
+						}
+						
+						div.copyright
+						{
+								text-align: center;
+						}
+
+						input.browse-input
+						{
+							width: 100%;
+							max-width: 500px;
+														
+							resize: none;
+
+							height: 100%;
+						}						
+    /**/
+    </style>
+    <title>
+      Search Patient Names
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style type="text/css">
+    </style>
+  </head>
+	  <script>
+/*	  
+		  defaultScrollWidth = 0;
+		  
+		  function auto_grow(element) {
+			element.style.height = "5px";
+			element.style.height = (element.scrollHeight*4)+"px";
+
+			if (defaultScrollWidth == 0) {
+				defaultScrollWidth = element.scrollWidth; //i.e. 42% of the width of the full width of the Browser Window
+				alert("defaultScrollWidth: "+defaultScrollWidth);
+			}
+			else if (element.scrollWidth < defaultScrollWidth){
+//				defaultScrollWidth = 100%;
+				defaultScrollWidth = element.scrollWidth;
+//				alert("defaultScrollWidth: "+defaultScrollWidth);
+
+			}
+				
+			element.style.width = defaultScrollWidth; //(element.scrollWidth+element.scrollWidth*0.42)+"px";			
+		  }
+*/
+	  </script>
+  <body>
+	<h2>
+	Search Patient Names
+	</h2>
+	<span>
+	</span>
+	<!-- Form -->
+	<form id="browse-form" method="post" action="<?php echo site_url('browse/confirm')?>">
+		<?php
+			$itemCounter = 1;
+		?>
+<!--		<input type="hidden" name="reportTypeIdParam" value="1" required>
+		<input type="hidden" name="reportTypeNameParam" value="Incident Report" required>
+-->
+
+		<div>
+			<table width="100%">
+<!--
+			  <tr>
+				<td>
+				  <b><span>Pangalan</span></b>
+				</td>
+			  </tr>
+-->
+			  <tr>
+				<td>				
+				  <input type="text" class="browse-input" placeholder="" name="nameParam" required>
+				</td>
+			  </tr>
+			</table>
+		</div>
+		<br />
+		<br />		
+		<!-- Buttons -->
+		<button type="submit" class="Button-login">
+			Submit
+		</button>
+	</form>
+	<br />
+	<br />
+	<br />
+	<br />
+
+	<div class="copyright">
+		<span>© Usbong Social Systems, Inc. 2011~2020. All rights reserved.</span>
+	</div>		 
+  </body>
+</html>
