a94529e2a17123bf9b28bc02a159734de169a4a6
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-03-10 14:43:57 +0800

    Add files via upload

--- usbong_kms/application/models/Browse_Model.php
+++ usbong_kms/application/models/Browse_Model.php
@@ -12,14 +12,72 @@ class Browse_Model extends CI_Model
 		
 		$query = $this->db->get('report');
 */
-		$this->db->select('patient_name');//, patient_id');
-		$this->db->distinct();
+		$this->db->select('patient_name', 'patient_id');
+		$this->db->distinct('patient_name');
 		$this->db->like('patient_name', $param['nameParam']);
 //		$this->db->order_by('added_datetime_stamp`', 'DESC');//ASC');
 		$this->db->limit(8);//1);
 		
 		$query = $this->db->get('patient');
 
+//		$row = $query->row();		
+		$rowArray = $query->result_array();
+		
+		if ($rowArray == null) {			
+			return False; //edited by Mike, 20190722
+		}
+		
+//		echo "report_id: ".$rowArray[0]['report_id'];
+		
+/*		return $row->report_description;
+*/
+//		return $rowArray[0]['report_description'];
+
+		return $rowArray;
+	}	
+
+	public function getDetailsListViaName($param) 
+	{		
+//		echo "reportTypeNameParam: ".$param['reportTypeNameParam'];
+/*	
+		$this->db->select('report_description, report_id');
+//		$this->db->where('report_description', $param['name']);		
+		$this->db->order_by('added_datetime_stamp`', 'DESC');//ASC');
+		$this->db->limit(8);//1);
+		
+		$query = $this->db->get('report');
+*/
+/*
+		$this->db->select('patient_name', 'patient_id');
+		$this->db->distinct('patient_name');
+		$this->db->like('patient_name', $param['nameParam']);
+//		$this->db->order_by('added_datetime_stamp`', 'DESC');//ASC');
+		$this->db->limit(8);//1);
+		
+		$query = $this->db->get('patient');
+*/
+
+/*
+		$this->db->select('t1.transaction_date, t1.fee, t1.transaction_type_name, t2.patient_name');
+		$this->db->from('transaction as t1');
+		$this->db->join('patient as t2', 't1.patient_id = t2.patient_id', 'LEFT');
+		$this->db->distinct('t2.patient_name');
+		$this->db->like('patient_name', $param['nameParam']);
+		$this->db->order_by('t1.transaction_date', 'DESC');//ASC');
+		$this->db->limit(8);//1);
+		
+		$query = $this->db->get('transaction');
+*/
+
+		$this->db->select('t1.patient_name, t2.transaction_date, t2.fee, t2.transaction_type_name, t2.treatment_type_name');
+		$this->db->from('patient as t1');
+		$this->db->join('transaction as t2', 't1.patient_id = t2.patient_id', 'LEFT');
+		$this->db->distinct('t1.patient_name');
+		$this->db->like('t1.patient_name', $param['nameParam']);
+		$this->db->order_by('t2.transaction_date', 'DESC');//ASC');
+		$this->db->limit(8);//1);
+		
+		$query = $this->db->get('patient');
 
 
 //		$row = $query->row();		
