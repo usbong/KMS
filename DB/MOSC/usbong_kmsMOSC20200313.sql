bec0637267bc738b5a2c73a9c22e163c334811c1
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-03-13 13:11:10 +0800

    Add files via upload

---
+++ DB/MOSC/usbong_kmsMOSC20200313.sql
@@ -0,0 +1,209 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 13, 2020 at 06:09 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `usbong_kms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `patient_id` int(11) NOT NULL,
+  `patient_name` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `report_id` int(11) NOT NULL,
+  `report_type_id` tinyint(4) NOT NULL DEFAULT 2,
+  `report_filename` text NOT NULL,
+  `report_description` text NOT NULL,
+  `added_datetime_stamp` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report_type`
+--
+
+CREATE TABLE `report_type` (
+  `report_type_id` tinyint(4) NOT NULL,
+  `report_type_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `report_type`
+--
+
+INSERT INTO `report_type` (`report_type_id`, `report_type_name`) VALUES
+(1, 'Lessons-learned Report'),
+(2, 'OT and PT Treatment'),
+(3, 'Incident Report'),
+(4, 'Reports from All Locations'),
+(5, 'Report Image'),
+(6, 'Consultation');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `transaction_id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `transaction_date` text NOT NULL,
+  `fee` int(11) NOT NULL,
+  `transaction_type_id` int(11) NOT NULL,
+  `transaction_type_name` text NOT NULL,
+  `treatment_type_name` text NOT NULL,
+  `treatment_diagnosis` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_type`
+--
+
+CREATE TABLE `transaction_type` (
+  `transaction_type_id` int(11) NOT NULL,
+  `transaction_type_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaction_type`
+--
+
+INSERT INTO `transaction_type` (`transaction_type_id`, `transaction_type_name`) VALUES
+(1, 'CASH'),
+(2, 'SC'),
+(3, 'PWD'),
+(4, 'HMO/ETIQA'),
+(5, 'HMO/AVEGA'),
+(6, 'HMO/BLUECROSS'),
+(7, 'HMO/CAREHEALTHPLUS'),
+(8, 'HMO/COCOLIFE'),
+(9, 'HMO/EASTWEST'),
+(10, 'HMO/GENERALIPHIL'),
+(11, 'HMO/GETWELL'),
+(12, 'HMO/HMI'),
+(13, 'HMO/INTELLICARE'),
+(14, 'HMO/LACSON-LACSON'),
+(15, 'HMO/MAXICARE'),
+(16, 'HMO/MEDASIA'),
+(17, 'HMO/MEDICARD'),
+(18, 'HMO/MEDOCARE'),
+(19, 'HMO/PHILCARE'),
+(20, 'HMO/VALUCARE');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patient_id`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `payslip_type_id` (`report_type_id`);
+
+--
+-- Indexes for table `report_type`
+--
+ALTER TABLE `report_type`
+  ADD PRIMARY KEY (`report_type_id`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`transaction_id`);
+
+--
+-- Indexes for table `transaction_type`
+--
+ALTER TABLE `transaction_type`
+  ADD PRIMARY KEY (`transaction_type_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report_type`
+--
+ALTER TABLE `report_type`
+  MODIFY `report_type_id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaction_type`
+--
+ALTER TABLE `transaction_type`
+  MODIFY `transaction_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`report_type_id`) REFERENCES `report_type` (`report_type_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
