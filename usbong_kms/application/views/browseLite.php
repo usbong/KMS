1572f5094a5af80b0c14d54f70a986e866b3e98f
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-03-15 06:57:15 +0800

    Add files via upload

---
+++ usbong_kms/application/views/browseLite.php
@@ -0,0 +1,337 @@
+<!--
+' Copyright 2020 Usbong Social Systems, Inc.
+'
+' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+'
+' http://www.apache.org/licenses/LICENSE-2.0
+'
+' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+'
+' @author: Michael Syson
+' @date created: 20200306
+' @date updated: 20200314
+-->
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset="utf-8">
+
+    <!-- Reference: Apache Friends Dashboard index.html -->
+    <!-- "Always force latest IE rendering engine or request Chrome Frame" -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+    <style type="text/css">
+	/**/
+	                    body
+                        {
+                                font-family: Arial;
+								font-size: 11pt
+                        }
+						
+						div.checkBox
+						{
+								border: 1.5pt solid black; height: 9pt; width: 9pt;
+								text-align: center;
+								float: left
+						}
+						
+						div.option
+						{
+								padding: 2pt;
+								display: inline-block;
+						}
+						
+						div.copyright
+						{
+								text-align: center;
+						}
+
+						input.browse-input
+						{
+							width: 100%;
+							max-width: 500px;
+														
+							resize: none;
+
+							height: 100%;
+						}	
+
+						img.Image-companyLogo {
+							max-width: 60%;
+							height: auto;
+						}
+						
+						table.search-result
+						{
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}						
+
+						td.column
+						{
+							border: 1px dotted #ab9c7d;		
+						}						
+						
+    /**/
+    </style>
+    <title>
+      Search Patient Names
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style type="text/css">
+    </style>
+  </head>
+	  <script>
+		function copyText(iCount){
+//			alert("hello"+iCount);
+	 
+			//Reference: https://stackoverflow.com/questions/51625169/click-on-text-to-copy-a-link-to-the-clipboard;
+			//last accessed: 20200307
+			//answer by: colxi on 20180801; edited by: Lord Nazo on 20180801	 
+/*	 
+			var holdText = document.getElementById("patientNameId"+iCount).innerText;
+
+			const el = document.createElement('textarea');
+		    el.value = holdText;
+			document.body.appendChild(el);
+			el.select();
+			document.execCommand('copy');
+			document.body.removeChild(el);
+
+			//alert("text: "+holdText);
+*/
+			var sHoldTextPatientName = document.getElementById("patientNameId"+iCount).innerText;
+			var sHoldTextFee = document.getElementById("feeId"+iCount).innerText; //.innerText;
+
+//			alert("sHoldTextPatientName: "+sHoldTextPatientName);
+//			alert("sHoldTextFee: "+sHoldTextFee);
+
+			var sHoldTextTransactionTypeName = document.getElementById("transactionTypeNameId"+iCount).innerText;
+
+			var sTreatmentTypeName = document.getElementById("treatmentTypeNameId"+iCount).innerText;
+
+			var sDiscountAmount = "";
+			var sTotalAmount = "0";
+			
+			if (sHoldTextTransactionTypeName=="CASH") {
+				//alert("CASH!");
+				sTotalAmount = sHoldTextFee;
+			}
+			else if (sHoldTextTransactionTypeName=="SC/PWD") {
+				//note: solve the values of the other variables using one (1) known variable value
+				sTotalAmount = sHoldTextFee
+				sHoldTextFee = -sHoldTextFee/(0.20-1);
+				sDiscountAmount = "" + sHoldTextFee*0.20;
+			}
+			else if (sHoldTextTransactionTypeName=="NC") {
+				sHoldTextFee = "NC";				
+				sTotalAmount = "NC";				
+			}						
+			else { //hmo
+				sHoldTextFee = "HMO";				
+				sTotalAmount = sHoldTextTransactionTypeName.toLowerCase();				
+			}
+			
+			const el = document.createElement('textarea');
+/*		    
+			el.value = sHoldTextPatientName+ "\t" + sHoldTextFee + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + sDiscountAmount + "\t" + sTotalAmount;
+			document.body.appendChild(el);
+*/			
+
+			sTreatmentTypeName = sTreatmentTypeName.toUpperCase();
+			
+			if ((sTreatmentTypeName=="SWT") || (sTreatmentTypeName=="SHOCKWAVE")) {
+				el.value = sHoldTextPatientName + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" +sHoldTextFee + "\t" + "\t" + sDiscountAmount + "\t" + sTotalAmount;
+			}
+			else if (sTreatmentTypeName=="LASER") {
+				el.value = sHoldTextPatientName + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" +sHoldTextFee + "\t" + "\t" + sDiscountAmount + "\t" + "\t" + sTotalAmount;
+			}
+			else if (sTreatmentTypeName=="OT") {
+				el.value = sHoldTextPatientName + "\t" + "\t" + sHoldTextFee + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + sDiscountAmount + "\t" + sTotalAmount;
+			}
+			else if (sTreatmentTypeName=="IN-PT") {
+				el.value = sHoldTextPatientName + "\t" + "\t" + "\t" + "\t" + sHoldTextFee + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + sDiscountAmount + "\t" + sTotalAmount;
+			}
+			else {
+				el.value = sHoldTextPatientName+ "\t" + sHoldTextFee + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + sDiscountAmount + "\t" + sTotalAmount;
+			}
+			
+			document.body.appendChild(el);							
+			el.select();
+			document.execCommand('copy');
+			document.body.removeChild(el);
+
+//			alert("text: "+sHoldTextPatientName + sHoldTextFee);//el.value);
+
+		}
+/*	  
+		  defaultScrollWidth = 0;
+		  
+		  function auto_grow(element) {
+			element.style.height = "5px";
+			element.style.height = (element.scrollHeight*4)+"px";
+
+			if (defaultScrollWidth == 0) {
+				defaultScrollWidth = element.scrollWidth; //i.e. 42% of the width of the full width of the Browser Window
+				alert("defaultScrollWidth: "+defaultScrollWidth);
+			}
+			else if (element.scrollWidth < defaultScrollWidth){
+//				defaultScrollWidth = 100%;
+				defaultScrollWidth = element.scrollWidth;
+//				alert("defaultScrollWidth: "+defaultScrollWidth);
+
+			}
+				
+			element.style.width = defaultScrollWidth; //(element.scrollWidth+element.scrollWidth*0.42)+"px";			
+		  }
+*/
+	  </script>
+  <body>
+	<table>
+	  <tr>
+		<td>				
+			<img class="Image-companyLogo" src="<?php echo base_url('assets/images/usbongLogo.png');?>">	
+		</td>
+		<td>				
+			<h2>
+				Search Patient Names
+			</h2>
+		</td>
+	  </tr>
+	</table>
+	<span>
+	</span>
+	<!-- Form -->
+	<form id="browse-form" method="post" action="<?php echo site_url('browseLite/confirm')?>">
+		<?php
+			$itemCounter = 1;
+		?>
+<!--		<input type="hidden" name="reportTypeIdParam" value="1" required>
+		<input type="hidden" name="reportTypeNameParam" value="Incident Report" required>
+-->
+
+		<div>
+			<table width="100%">
+<!--
+			  <tr>
+				<td>
+				  <b><span>Pangalan</span></b>
+				</td>
+			  </tr>
+-->
+			  <tr>
+				<td>				
+				  <input type="text" class="browse-input" placeholder="" name="nameParam" required>
+				</td>
+			  </tr>
+			</table>
+		</div>
+		<br />
+		<!-- Buttons -->
+		<button type="submit" class="Button-login">
+			Enter
+		</button>
+	</form>
+	<br />
+	<br />
+
+<!--	<div id="myText" onclick="copyText(1)">Text you want to copy</div>
+-->	
+	<?php
+		//get only name strings from array 
+		if (isset($result)) {			
+			if ($result!=null) {		
+				$resultCount = count($result);
+				if ($resultCount==1) {
+					echo '<div>Showing <b>'.count($result).'</b> result found.</div>';
+				}
+				else {
+					echo '<div>Showing <b>'.count($result).'</b> results found.</div>';			
+				}			
+
+				echo "<br/>";
+				echo "<table class='search-result'>";
+				
+				$iCount = 1;
+				foreach ($result as $value) {
+		//			echo $value['report_description'];			
+	/*	
+					echo $value['patient_name'];				
+					echo "<br/><br/>";
+	*/
+		?>				
+		
+					  <tr class="row">
+						<td class ="column">				
+							<a href="#" id="patientNameId<?php echo $iCount?>" onclick="copyText(<?php echo $iCount?>)">
+								<div>
+				<?php
+								echo $value['patient_name'];
+				?>		
+								</div>								
+							</a>
+						</td>
+						<td class ="column">				
+								<div id="transactionDateId<?php echo $iCount?>">
+							<?php
+								echo $value['transaction_date'];
+							?>
+								</div>
+						</td>
+						<td class ="column">				
+<!--								<span id="99">
+-->
+								<span id="feeId<?php echo $iCount?>">
+							<?php
+								echo $value['fee'];
+							?>
+								</span>
+						</td>
+						<td class ="column">				
+								<div id="transactionTypeNameId<?php echo $iCount?>">
+							<?php
+								echo $value['transaction_type_name'];
+							?>
+								</div>
+						</td>
+						<td class ="column">				
+								<div id="treatmentTypeNameId<?php echo $iCount?>">
+							<?php
+								echo $value['treatment_type_name'];
+							?>
+								</div>
+						</td>
+					  </tr>
+		<?php				
+					$iCount++;		
+//					echo "<br/>";
+				}				
+				
+				echo "</table>";				
+				echo "<br/>";				
+				echo '<div>***NOTHING FOLLOWS***';	
+			}
+			else {					
+				echo '<div>';					
+				echo 'Your search <b>- '.$nameParam.' -</b> did not match any of our patients\' names.';
+				echo '<br><br>Recommendation:';
+				echo '<br>&#x25CF; Reverify that the patient is <b>not</b> new.';				
+				echo '<br>&#x25CF; Reverify that the spelling is correct.';				
+				echo '</div>';					
+			}			
+		}
+	?>
+	<br />
+	<br />
+	<br />
+	<br />
+	<div class="copyright">
+		<span>© Usbong Social Systems, Inc. 2011~2020. All rights reserved.</span>
+	</div>		 
+  </body>
+</html>
