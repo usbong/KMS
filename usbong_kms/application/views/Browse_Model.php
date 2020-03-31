f5ce2ec59a840e99d174ace39da56ead1313a117
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-04-01 05:09:00 +0800

    Add files via upload

--- usbong_kms/application/views/Browse_Model.php
+++ usbong_kms/application/views/Browse_Model.php
@@ -116,12 +116,16 @@ class Browse_Model extends CI_Model
 */
 /*
 		$this->db->select('t1.item_name, t1.item_price');
+
 		$this->db->from('item as t1');
 //		$this->db->join('transaction as t2', 't1.patient_id = t2.patient_id', 'LEFT');
 //		$this->db->join('medical_doctor as t3', 't2.medical_doctor_id = t3.medical_doctor_id', 'LEFT');
+
 //		$this->db->distinct('t1.patient_name');
 		$this->db->group_by('t1.item_name');
+
 		$this->db->where('t1.item_type_id', 1); //1 = Medicine
+
 		$this->db->like('t1.item_name', $param['nameParam']);
 //		$this->db->order_by('t2.transaction_date', 'DESC');//ASC');
 		$this->db->limit(8);//1);
@@ -141,9 +145,8 @@ class Browse_Model extends CI_Model
 
 		$this->db->like('item_name', $param['nameParam']);
 //		$this->db->order_by('t2.transaction_date', 'DESC');//ASC');
-/*
 		$this->db->limit(8);//1);
-*/		
+		
 		$query = $this->db->get('item');
 
 //		$row = $query->row();		
@@ -160,6 +163,36 @@ class Browse_Model extends CI_Model
 //		return $rowArray[0]['report_description'];
 		
 		return $rowArray;
+	}	
+
+	//added by Mike, 20200330
+	public function addTransactionMedicinePurchase($param) 
+	{		
+		$this->db->select('item_price');
+		$this->db->where('item_id', $param['itemId']);
+		$query = $this->db->get('item');
+		$row = $query->row();
+	
+		$data = array(
+					'patient_id' => -1,
+					'item_id' => $param['itemId'],
+					'transaction_date' => $param['transactionDate'],
+					'medical_doctor_id' => -1,
+					'fee' => $param['quantity'] * $row->item_price,
+					'transaction_type_name' => "CASH",
+					'report_id' => -1,
+					'notes' => "UNPAID"
+				);
+			
+		$this->db->insert('transaction', $data);
+		return $this->db->insert_id();
+	}	
+
+	//added by Mike, 20200331
+	public function deleteTransactionMedicinePurchase($param) 
+	{			
+        $this->db->where('transaction_id',$param['transactionId']);
+        $this->db->delete('transaction');
 	}	
 
 	public function getDetailsListViaId($nameId) 
@@ -183,18 +216,24 @@ class Browse_Model extends CI_Model
 		return $rowArray;
 	}	
 
-	//added by Mike, 20200328
+	//added by Mike, 20200328; edited by Mike, 20200330
 	public function getMedicineDetailsListViaItemId($itemId) 
 	{		
-		$this->db->select('t1.item_name, t1.item_price, t1.item_id, t2.transaction_id, t2.transaction_date');
+		$this->db->select('t1.item_name, t1.item_price, t1.item_id, t2.transaction_id, t2.transaction_date, t2.fee');
 		$this->db->from('item as t1');
 		$this->db->join('transaction as t2', 't1.item_id = t2.item_id', 'LEFT');
 		$this->db->distinct('t1.item_name');
 //		$this->db->like('t1.patient_name', $param['nameParam']);
-		$this->db->where('t1.item_id', $itemId);		
+
+		$this->db->where('t1.item_id', $itemId);
+
+/*
+		$this->db->where('t2.transaction_date', date("m/d/Y"));//ASC');
+*/		
 //		$this->db->where('t2.transaction_date!=', 0);		
 
 		$this->db->order_by('t2.transaction_date', 'DESC');//ASC');
+//		$this->db->limit(1);
 		
 		$query = $this->db->get('item');
 
@@ -207,5 +246,38 @@ class Browse_Model extends CI_Model
 		
 		return $rowArray;
 	}		
+	
+	//added by Mike, 20200328; edited by Mike, 20200331
+	public function getMedicineDetailsListViaNotesUnpaid() 
+	{		
+		$this->db->select('t1.item_name, t1.item_price, t1.item_id, t2.transaction_id, t2.transaction_date, t2.fee');
+		$this->db->from('item as t1');
+		$this->db->join('transaction as t2', 't1.item_id = t2.item_id', 'LEFT');
+		$this->db->distinct('t1.item_name');
+//		$this->db->like('t1.patient_name', $param['nameParam']);
+/*
+		$this->db->where('t1.item_id', $itemId);
+*/
+
+		$this->db->where('t2.transaction_date', date("m/d/Y"));//ASC');
+		$this->db->like('t2.notes', "UNPAID");
+		
+//		$this->db->where('t2.transaction_date!=', 0);		
+
+		$this->db->order_by('t2.transaction_date', 'DESC');//ASC');
+//		$this->db->limit(1);
+		
+		$query = $this->db->get('item');
+
+//		$row = $query->row();		
+		$rowArray = $query->result_array();
+		
+		if ($rowArray == null) {			
+			return False; //edited by Mike, 20190722
+		}
+		
+		return $rowArray;
+	}		
+
 }
 ?>
