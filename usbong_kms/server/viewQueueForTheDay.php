048ee3e0b57d968a306fd07218c2b6b5e9c60271
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-05-03 11:10:33 +0800

    Add files via upload

---
+++ usbong_kms/server/viewQueueForTheDay.php
@@ -0,0 +1,43 @@
+<?php
+/*
+  Copyright 2020 Usbong Social Systems, Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+
+  @author: Michael Syson
+  @date created: 20200503
+  @date updated: 20200503
+
+  Input:
+  1) Patients list in Tab-delimited .txt file from the Information Desk
+
+  Output:
+  1) Patients list viewable on a Computer Web Browser  
+  
+  Computer Web Browser Address (Example):
+  1) http://localhost/usbong_kms/server/viewQueueForTheDay.php
+
+*/
+	date_default_timezone_set('Asia/Hong_Kong');
+
+	//added by Mike, 20191017
+	$dateToday = (new DateTime())->format('Y-m-d');
+
+	//TO-DO: -update: file location
+	$filename="C:/Usbong/Patients".$dateToday.".txt";
+	
+	echo $filename;
+	
+	echo "<br/>";
+	echo "<br/>";
+	
+	$fileContents = file_get_contents($filename);
+
+	//TO-DO: -add: display as Tab-delimited
+	echo $fileContents;
+	
+?>
