ff553cf2e2c64ac1537298754269be8798a17b84
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-05-22 17:00:01 +0800

    Add files via upload

---
+++ usbong_kms/server/viewSummaryReportForTheDay.php
@@ -0,0 +1,170 @@
+<!--
+  Copyright 2020 Usbong Social Systems, Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+
+  @author: Michael Syson
+  @date created: 20200522
+  @date updated: 20200522
+
+  Input:
+  1) Summary Worksheet with counts and amounts in .csv (comma-separated value) file at the Accounting/Cashier Unit
+
+  Output:
+  1) Summary Worksheet that is viewable on a Computer Web Browser  
+  
+  Note:
+  1) We can reuse this set of instructions with other .csv files that need to be viewable on a Computer Web Browser.
+  2) We can auto-generate the .csv files using Microsoft EXCEL and LibreOffice CALC.
+	
+  Computer Web Browser Address (Example):
+  1) http://localhost/usbong_kms/server/viewSummaryReportForTheDay.php   
+-->
+<?php
+//defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset="utf-8">
+
+    <!-- Reference: Apache Friends Dashboard index.html -->
+    <!-- "Always force latest IE rendering engine or request Chrome Frame" -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+    <style type="text/css">
+	/**/
+	                    body
+                        {
+							font-family: Arial;
+							font-size: 11pt;
+
+							/* This makes the width of the output page that is displayed on a browser equal with that of the printed page. */
+							/* Legal Size; Landscape*/							
+							width: 802px; /* 670px */
+                        }
+						
+						div.copyright
+						{
+							text-align: center;
+						}
+						
+						img.Image-companyLogo {
+							max-width: 60%;
+							height: auto;
+							float: left;
+							text-align: center;
+							padding-left: 20px;
+							padding-top: 10px;
+						}
+
+						img.Image-moscLogo {
+							max-width: 20%;
+							height: auto;
+							float: left;
+							text-align: center;
+						}
+						
+						table.imageTable
+						{
+							width: 100%;
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}						
+
+						td.tableHeaderColumn
+						{
+							background-color: #00ff00; <!--#93d151; lime green-->
+							border: 1pt solid #00ff00;		
+							text-align: center;
+							font-weight: bold;
+						}						
+
+						td.column
+						{
+							border: 1px dotted #ab9c7d;		
+							text-align: center;
+						}						
+						
+						td.imageColumn
+						{
+							width: 40%;
+							display: inline-block;
+						}						
+
+						td.pageNameColumn
+						{
+							width: 50%;
+							display: inline-block;
+							text-align: right;
+						}						
+    /**/
+    </style>
+    <title>
+      Summary Report for the Day (MOSC)
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style type="text/css">
+    </style>
+  </head>
+	  <script>
+	  </script>
+  <body>
+<?php
+	date_default_timezone_set('Asia/Hong_Kong');
+
+	$dateToday = (new DateTime())->format('Y-m-d');
+
+	//TO-DO: -update: file location
+//	$filename="C:/Usbong/Patients".$dateToday.".txt";	
+//	$filename="D:\Usbong\MOSC\Forms\Information Desk\output\cashier\summaryReport".$dateToday.".txt";
+	$filename="D:\Usbong\MOSC\Forms\Information Desk\output\cashier\summaryReport".$dateToday.".csv";
+
+	echo "<br/>";
+	echo "<table>";
+				
+	//TO-DO: -add: auto-identify and update date format to use YYYY-MM-DD
+	
+	echo '<tr class="row">';
+
+	ini_set('auto_detect_line_endings', true);
+
+	//Reference: https://stackoverflow.com/questions/9139202/how-to-parse-a-csv-file-using-php;
+	//answer by: thenetimp, 20120204T0730
+	//edited by: thenetimp, 20170823T1704
+
+	$row = 1;
+	//if (($handle = fopen("test.csv", "r")) !== FALSE) {
+	if (($handle = fopen($filename, "r")) !== FALSE) {
+	  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+		$num = count($data) -1; //we add -1 for the computer to not include the excess cell due to the ending \n
+	//    echo "<p> $num fields in line $row: <br /></p>\n";
+		$row++;
+		for ($c=0; $c < $num; $c++) {
+	//        echo $data[$c] . "<br />\n";
+			echo "<td class='column'>".utf8_encode($data[$c])."</td>";
+		}
+		echo '</tr><tr class="row">';
+	  }
+	  echo '</tr>';
+	  
+	  fclose($handle);
+	}
+
+?>
+	</table>
+	<br />
+	<div>***NOTHING FOLLOWS***</div>
+	<br />
+	<br />
+	<div class="copyright">
+		<span>© Usbong Social Systems, Inc. 2011~2020. All rights reserved.</span>
+	</div>		 
+  </body>
+</html>
