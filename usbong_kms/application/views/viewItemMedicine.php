e44c62f9a436cef9fb908f1945d2b723f3e12f37
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-06-07 19:58:02 +0800

    Add files via upload

--- usbong_kms/application/views/viewItemMedicine.php
+++ usbong_kms/application/views/viewItemMedicine.php
@@ -9,7 +9,7 @@
 '
 ' @author: Michael Syson
 ' @date created: 20200306
-' @date updated: 20200603
+' @date updated: 20200607
 -->
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
