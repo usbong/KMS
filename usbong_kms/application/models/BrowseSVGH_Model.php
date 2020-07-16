9fdb50c73b4653a1f360067f8419d07643893d1e
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-07-16 17:26:31 +0800

    Add files via upload

--- usbong_kms/application/models/BrowseSVGH_Model.php
+++ usbong_kms/application/models/BrowseSVGH_Model.php
@@ -162,11 +162,17 @@ class BrowseSVGH_Model extends CI_Model
 	public function getNewestPatientDetailsListViaName($param) 
 	{		
 		//we use this at MOSC
+/*		//edited by Mike, 20200716
 		$this->db->select('t1.patient_name, t1.patient_id, t2.transaction_id, t2.transaction_date, t2.fee, t2.transaction_type_name, t2.treatment_type_name, t2.treatment_diagnosis, t3.medical_doctor_name, t3.medical_doctor_id');
+*/		
+		//we use this at MOSC
+		$this->db->select('t1.patient_name, t1.patient_id, t2.transaction_id, t2.transaction_date, t2.fee, t2.transaction_type_name, t2.treatment_type_name, t2.treatment_diagnosis, t2.notes, t3.medical_doctor_name, t3.medical_doctor_id, t4.treatment_datetime_stamp, t4.treatment_diagnosis, t4.treatment_temperature, t4.treatment_bp, t4.therapist_id');
+		
 
 		$this->db->from('patient as t1');
 		$this->db->join('transaction as t2', 't1.patient_id = t2.patient_id', 'LEFT');
 		$this->db->join('medical_doctor as t3', 't2.medical_doctor_id = t3.medical_doctor_id', 'LEFT');
+		$this->db->join('treatment as t4', 't2.treatment_id = t4.treatment_id', 'LEFT');
 
 //		$this->db->distinct('t1.patient_name');
 //		$this->db->group_by('t1.patient_name');
