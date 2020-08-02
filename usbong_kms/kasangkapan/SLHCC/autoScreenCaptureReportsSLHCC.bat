71f940a9c9d8c6ae2474d19f01a12be523e01929
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-08-02 09:29:13 +0800

    Add files via upload

---
+++ usbong_kms/kasangkapan/SLHCC/autoScreenCaptureReportsSLHCC.bat
@@ -0,0 +1,10 @@
+phantomjs saveWebPageAsImageFileSLHCC.js MonthlyStatisticsConsultation
+phantomjs saveWebPageAsImageFileSLHCC.js MonthlyStatisticsProcedure
+phantomjs saveWebPageAsImageFileSLHCC.js viewReportNonMedicineUnified
+phantomjs saveWebPageAsImageFileSLHCC.js MonthlyStatisticsTreatment
+phantomjs saveWebPageAsImageFileSLHCC.js MonthlySummaryReportOutputConsultation
+phantomjs saveWebPageAsImageFileSLHCC.js MonthlySummaryReportOutputTreatment
+
+set myDate=%date:~10,4%%date:~4,2%%date:~7,2%
+
+explorer "C:\xampp\htdocs\usbong_kms\kasangkapan\phantomjs-2.1.1-windows\bin\output\SLHCC\"%myDate%
