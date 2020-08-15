1d721725ef8095f39a4f55c324b8411de1975c05
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-08-15 12:24:23 +0800

    Add files via upload

--- Miscellaneous Commands/usbongAutoStartWineLanMessenger.sh
+++ Miscellaneous Commands/usbongAutoStartWineLanMessenger.sh
@@ -1,1 +1,2 @@
-sh -c "cd /home/unit_member/.wine/drive_c/'Program Files (x86)'/'LAN Messenger'/; wine /home/unit_member/.wine/drive_c/'Program Files (x86)'/'LAN Messenger'/lmc.exe"
+#!/bin/bash 
+sh -c "cd /home/unit_member/.wine/drive_c/'Program Files (x86)'/'LAN Messenger'/; wine /home/unit_member/.wine/drive_c/'Program Files (x86)'/'LAN Messenger'/lmc.exe&"
