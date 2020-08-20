255ccdc4c16c0a6744d7c5d1176d7b32d37625e6
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-08-20 09:06:27 +0800

    Add files via upload

--- usbong_kms/server/getClientMachineAddress.php
+++ usbong_kms/server/getClientMachineAddress.php
@@ -9,7 +9,6 @@
   @date updated: 20200819
   Input:
   1) Client computer connected to the computer network
-
   Output:
   1) Client computer's Machine (MAC) Address
   
@@ -21,7 +20,6 @@
 /*
 	// connect to the database
 	include('usbong-kms-connect.php');
-
 	$mysqli->set_charset("utf8");
 */	
 	date_default_timezone_set('Asia/Hong_Kong');
@@ -33,18 +31,41 @@
 */
 
 	
-	//TO-DO: -reverify: this
-	//$ipAddress=$_SERVER['REMOTE_ADDR'];
+	$ipAddress=$_SERVER['REMOTE_ADDR'];
 
-	$macAddress = exec('getmac');
-	//echo system('getmac')."<br/>";
+	//removed by Mike, 20200820
+	//$macAddress = exec('getmac');
+	////echo system('getmac')."<br/>";
+	//$macAddress = explode(" ",$macAddress)[0];
 
-	$macAddress = explode(" ",$macAddress)[0];
+	echo "Output:";
+	echo "<br/>";
+	echo "Your Internet Protocol (IP) Address: ".$ipAddress;
+	echo "<br/>";
+	echo "Your Machine Address: "; //.$macAddress;	
 
-	//echo $ipAddress;
-	//echo "<br/>";
-	echo "Your Machine Address Output: ".$macAddress;	
-	
+	//note: output is blank if Windows Machine
+	//We use this set of instructions with Linux Machines
+	//Reference: https://stackoverflow.com/questions/1420381/how-can-i-get-the-mac-and-the-ip-address-of-a-connected-client-in-php;
+	//last accessed: 20200820
+	//answer by: Paul Dixon, 20090914T0848
+
+	#run the external command, break output into lines
+	$arp=`arp -a $ipAddress`;
+	$lines=explode("\n", $arp);
+
+	#look for the output line describing our IP address
+	foreach($lines as $line)
+	{
+	   $cols=preg_split('/\s+/', trim($line));
+	   if ($cols[0]==$ipAddress)
+	   {
+		   $macAddress=$cols[1];
+		   echo $macAddress;
+	   }
+	}
+
+
 	//removed by Mike, 20200819
 /*
 	//close database connection
