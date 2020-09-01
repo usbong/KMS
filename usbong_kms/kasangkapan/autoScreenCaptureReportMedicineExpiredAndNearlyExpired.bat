fc546312a596c6e4cd41e95da304c031b0259c8d
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-09-01 09:49:37 +0800

    Add files via upload

---
+++ usbong_kms/kasangkapan/autoScreenCaptureReportMedicineExpiredAndNearlyExpired.bat
@@ -0,0 +1,5 @@
+phantomjs saveWebPageAsImageFile.js viewReportMedicineExpired
+
+set myDate=%date:~10,4%%date:~4,2%%date:~7,2%
+
+explorer "C:\xampp\htdocs\usbong_kms\kasangkapan\phantomjs-2.1.1-windows\bin\output\"%myDate%
