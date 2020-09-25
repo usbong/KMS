10bd92fe5a0ad54d2d256e3f7ecb26dce8f43f4e
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-09-25 15:41:14 +0800

    Add files via upload

---
+++ Miscellaneous Commands/saveDatabaseStorageCommandInLinuxPC.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright 2020 Usbong Social Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+#
+# Save Database Storage Command in Linux PC
+# @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
+# @author: SYSON, MICHAEL B.
+# @date created: 20200925
+# @last modified: 20200925
+
+#update source location
+#source: from Windows machine, e.g. Windows 7, Service Pack 1
+source="/run/user/1000/gvfs/smb-share:server=mosc-accounting,share=information%20desk/unified.bat"
+
+echo "source: ".$source
+
+#update destination location
+#destination: to Linux machine, e.g. LUBUNTU 20.04 LTS
+destination="/home/unit_member/Documents/halimbawa/"
+
+echo "destination: ".$destination
+
+#notes: rsync inputs
+#where: r = recursive
+#where: t = keep time
+#where: v = verbose
+rsync -rtv $source $destination
