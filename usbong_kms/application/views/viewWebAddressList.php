295f2529ec95048516d7ec364d3cbc24f0d81561
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-10-10 09:07:17 +0800

    Add files via upload

--- usbong_kms/application/views/viewWebAddressList.php
+++ usbong_kms/application/views/viewWebAddressList.php
@@ -1,15 +1,11 @@
 <!--
   Copyright 2020 Usbong Social Systems, Inc.
-
   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
-
   http://www.apache.org/licenses/LICENSE-2.0
-
   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
-
   @author: Michael Syson
   @date created: 20200602
-  @date updated: 20200827
+  @date updated: 20201010
 	
   Computer Web Browser Address (Example):
   1) http://192.168.11.10/usbong_kms/index.php/report/viewWebAddressList
@@ -17,7 +13,6 @@
   Note:
   1) Previously, we used this PHP page without the CodeIgniter framework
   http://192.168.11.10/usbong_kms/server/viewWebAddressList.php
-
   2) Update the file location
   
 -->
