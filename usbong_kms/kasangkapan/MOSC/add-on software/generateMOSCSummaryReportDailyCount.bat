c99e35e5ba7b9d5d05bc0c06032c697757317060
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-10-25 08:35:26 +0800

    Add files via upload

--- usbong_kms/kasangkapan/MOSC/add-on software/generateMOSCSummaryReportDailyCount.bat
+++ usbong_kms/kasangkapan/MOSC/add-on software/generateMOSCSummaryReportDailyCount.bat
@@ -1,3 +1,4 @@
+@echo off
 REM
 REM	Auto-generate MOSC Summary Report Daily Count
 REM Copyright 2020 Usbong Social Systems, Inc.
@@ -11,7 +12,7 @@ REM
 REM @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
 REM @author: SYSON, MICHAEL B.
 REM @date created: 20200306
-REM @last updated: 20201023
+REM @last updated: 20201025
 REM
 
 cd /d %1%
@@ -25,15 +26,18 @@ xcopy "tempListBeforeProcessing\*List.txt" "." /s /y
 
 cd %mainDirectory%
 		
-java -cp .\software;.\software\org.apache.commons.text.jar generateMOSCSummaryReportDailyCount input/cashier/*.txt assets/*.txt
-
+REM java -cp .\software;.\software\org.apache.commons.text.jar generateMOSCSummaryReportDailyCount input/cashier/*.txt assets/*.txt
+java -cp .\software;.\software\org.json.jar;.\software\org.apache.commons.text.jar generateMOSCSummaryReportDailyCount input/cashier/ assets/
 cd assets/transactions/
+
 del *List.txt
 
 rename *ListTemp.txt *List.txt
 
 cd %mainDirectory%
-cd ..
+
+REM removed by Mike, 20201025
+REM cd ..
 
 rem TO-DO: -update: this
 rem "add-on software"\requirements\"chrome.exe - Shortcut.lnk" file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputTreatment.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOfUnclassifiedDiagnosedCasesOutput.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputConsultation.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsConsultation.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsProcedure.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsTreatment.html
