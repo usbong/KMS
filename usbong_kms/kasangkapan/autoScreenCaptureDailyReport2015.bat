540a3da0ceacf35140476b740c17260668a533b8
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-10-28 11:00:39 +0800

    Add files via upload

---
+++ usbong_kms/kasangkapan/autoScreenCaptureDailyReport2015.bat
@@ -0,0 +1,5 @@
+phantomjs saveWebPageAsImageFile.js viewCSVFileMOSCDailyCount2015 -s
+
+set myDate=%date:~10,4%%date:~4,2%%date:~7,2%
+
+explorer "C:\xampp\htdocs\usbong_kms\kasangkapan\phantomjs-2.1.1-windows\bin\output\"%myDate%
