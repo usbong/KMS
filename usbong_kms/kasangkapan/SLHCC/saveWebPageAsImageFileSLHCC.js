38bb78b99e401ea19098370789f22482c8efc052
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-01-17 08:39:24 +0800

    Add files via upload

--- usbong_kms/kasangkapan/SLHCC/saveWebPageAsImageFileSLHCC.js
+++ usbong_kms/kasangkapan/SLHCC/saveWebPageAsImageFileSLHCC.js
@@ -1,14 +1,16 @@
 /*
 '
+' Copyright 2020~2021 USBONG SOCIAL SYSTEMS, INC. (USBONG)
 ' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
 '
 ' http://www.apache.org/licenses/LICENSE-2.0
 '
 ' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
 '
-' @author: Michael Syson
+' @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
+' @author: SYSON, MICHAEL B.
 ' @date created: 20200724
-' @date updated: 20210101
+' @date updated: 20210117
 '
 ' Reference:
 ' 1) https://phantomjs.org/; last accessed: 20200724
@@ -28,6 +30,8 @@ isFromAnnualReportAddSoftwareOutputFolder = system.args[2];
 //var webAddress = 'http://localhost/usbong_kms/index.php/REPORT/'; //default
 //edited by Mike, 20200830
 //var webAddress = 'file:///D:/Usbong/SLHCC/202007/'; //default
+//added by Mike, 20210117
+//TO-DO: -add: auto-set date year
 var webAddress = 'file:///D:/2020/add-on software/generateMonthlySummaryReport/add-on software/output/';
 
 var fileExtension = '.html'; //'';//edited by Mike, 20200802
@@ -65,8 +69,9 @@ page.open(webAddress+fileName+fileExtension, function(status) {
 	
 	//edited by Mike, 20200725
     //page.render('output/'+fileName+'.png');
-//	page.render('output/'+dateToday.toISOString()+'/'+fileName+'1.png');
-	page.render('output/SLHCC/'+dateToday.toISOString()+'/'+fileName+'1.png');
+	//edited by Mike, 20210117
+	page.render('output/'+dateToday.toISOString()+'/'+fileName+'1.png');
+//	page.render('output/SLHCC/'+dateToday.toISOString()+'/'+fileName+'1.png');
 
 /*	
     console.log("windowScreenHeight: " + window.screen.height);
