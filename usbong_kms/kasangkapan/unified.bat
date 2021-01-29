c704be724e5caac961d9f39229d4c381cc84c00c
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-01-29 11:09:30 +0800

    Add files via upload

---
+++ usbong_kms/kasangkapan/unified.bat
@@ -0,0 +1,7 @@
+start cmd /c "G:\Usbong MOSC\Everyone\Information Desk\viewAllReportsForTheDayUnified.bat"
+start cmd /c "G:\Usbong MOSC\Everyone\Information Desk\moscReportForTheDay.xlsm"
+
+cd "C:\xampp\htdocs\usbong_kms\kasangkapan\phantomjs-2.1.1-windows\bin\
+"C:\xampp\htdocs\usbong_kms\kasangkapan\phantomjs-2.1.1-windows\bin\autoScreenCaptureReports.bat"
+
+exit
