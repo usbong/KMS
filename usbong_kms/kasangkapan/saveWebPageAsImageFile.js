e7369f933d208468cdd22e0936a47f8ef8bd6dd4
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-01-29 11:07:01 +0800

    Add files via upload

--- usbong_kms/kasangkapan/saveWebPageAsImageFile.js
+++ usbong_kms/kasangkapan/saveWebPageAsImageFile.js
@@ -1,14 +1,15 @@
 /*
-'
+' Copyright 2020~2021 USBONG SOCIAL SYSTEMS, INC. (USBONG)
 ' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
 '
 ' http://www.apache.org/licenses/LICENSE-2.0
 '
 ' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
 '
-' @author: Michael Syson
+' @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
+' @author: SYSON, MICHAEL B.
 ' @date created: 20200724
-' @date updated: 20201127
+' @date updated: 20210129
 '
 ' Reference:
 ' 1) https://phantomjs.org/; last accessed: 20200724
@@ -72,7 +73,9 @@ page.open(webAddress+fileName+fileExtension, function(status) {
 	else {
 		//edited by Mike, 20201018
 //		page.render('output/'+dateToday.toISOString()+'/'+noonFolderName+fileName+'NoonReport1.png');
-		page.render('output/'+dateToday.toISOString()+'/'+noonFolderName+fileName+'Report1.png');
+		//edited by Mike, 20210129
+//		page.render('output/'+dateToday.toISOString()+'/'+noonFolderName+fileName+'Report1.png');
+		page.render('output/'+dateToday.toISOString()+'/'+noonFolderName+fileName+'ReportNoon.png');
 	}
 
 /*	
