123ddd1d2077d494afafcd65068d214704e98702
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-02-06 09:20:09 +0800

    Add files via upload

---
+++ usbong_kms/application/views/viewLabRequestForm.php
@@ -0,0 +1,288 @@
+<!--
+  Copyright 2020~2021 USBONG SOCIAL SYSTEMS, INC. (USBONG)
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+  @author: Michael Syson
+  @date created: 20200818
+  @date updated: 20210206
+
+  Input:
+  1) Laboratory Request Form (.csv format) at the Marikina Orthopedic Specialty Clinic (MOSC)
+  Output:
+  1) Laboratory Request Form that is viewable on a Computer Web Browser  
+  
+  Computer Web Browser Address (Example):
+  1) http://localhost/usbong_kms/server/viewCSVFileMOSCLabRequestForm.php   
+-->
+<?php
+//defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+   <!-- <meta charset="utf-8"> -->
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <!-- Reference: Apache Friends Dashboard index.html -->
+    <!-- "Always force latest IE rendering engine or request Chrome Frame" -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+    <style type="text/css">
+	/**/
+	                    body
+                        {
+							font-family: Arial;
+							font-size: 12pt;
+
+							/* This makes the width of the output page that is displayed on a browser equal with that of the printed page. */
+							/* Legal Size; Landscape*/							
+							width: 860px;/* 802px;*//* 670px */
+							
+							/* use zoom 67% scale*/
+							zoom: 67%; /* at present, command not support in Mozilla Firefox */				
+							transform: scale(0.67);
+							transform-origin: 0 0;							
+                        }
+						
+						div.copyright
+						{
+							text-align: center;
+						}
+						
+						img.Image-companyLogo {
+							max-width: 60%;
+							height: auto;
+							float: left;
+							text-align: center;
+							padding-left: 20px;
+							padding-top: 10px;
+						}
+
+						img.Image-moscLogo {
+							max-width: 20%;
+							height: auto;
+							float: left;
+							text-align: center;
+						}
+						
+						table.imageTable
+						{
+							width: 100%;
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}						
+
+						tr.rowEvenNumber {
+							background-color: #dddddd; <!--#dddddd; = gray #95b3d7; = sky blue; use as row background color-->
+							border: 1pt solid #00ff00;		
+						}
+
+						td.tableHeaderColumn
+						{
+							background-color: #00ff00; <!--#93d151; lime green-->
+							border: 1pt solid #00ff00;		
+							text-align: center;
+							font-weight: bold;
+						}						
+
+						td.column
+						{
+							border: 1px dotted #ab9c7d;		
+							text-align: center;
+						}						
+
+						td.columnBorderBottom
+						{
+							border: 1px dotted #ab9c7d;		
+							border-bottom: 4px double black;
+							text-align: center;
+						}						
+
+						td.columnBorderBottomDotted
+						{
+							border: 1px dotted #ab9c7d;		
+							border-bottom: 2px dotted black;
+							text-align: center;
+						}						
+
+						td.columnBorderTopBottom
+						{
+							border: 1px dotted #ab9c7d;		
+							border-top: 2px solid black;
+							border-bottom: 4px double black;
+							text-align: center;
+						}						
+						
+						td.imageColumn
+						{
+							width: 40%;
+							display: inline-block;
+						}						
+
+						td.pageNameColumn
+						{
+							width: 50%;
+							display: inline-block;
+							text-align: right;
+						}												
+    /**/
+    </style>
+    <title>
+      MARIKINA ORTHOPEDIC SPECIALTY CLINIC (MOSC): LABORATORY UNIT
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style type="text/css">
+    </style>
+  </head>
+	  <script>
+	  </script>
+  <body>
+<?php
+	date_default_timezone_set('Asia/Hong_Kong');
+	
+	//edited by Mike, 20200726
+	//$dateToday = (new DateTime())->format('Y-m-d');
+	$dateToday = Date('Y-m-d');
+
+	// connect to the database
+//	include('usbong-kms-connect.php');
+//	$mysqli->set_charset("utf8");
+	
+	//edited by Mike, 20210206
+//	$filename="C:\\xampp\\htdocs\\usbong_kms\\kasangkapan\\phantomjs-2.1.1-windows\\bin\\output\\2015\\201505~201507.csv";
+	$filename="G:\\Usbong MOSC\\Everyone\\Information Desk\\Laboratory\\templates\\MOSCLabRequestForm.csv";
+?>
+	<table class="imageTable">
+	  <tr>
+		<td class="imageColumn">				
+			<img class="Image-moscLogo" src="<?php echo base_url('assets/images/moscLogo.jpg');?>">		
+			<img class="Image-companyLogo" src="<?php echo base_url('assets/images/usbongLogo.png');?>">	
+		</td>
+		<td class="pageNameColumn">
+			<h3>
+				MARIKINA ORTHOPEDIC SPECIALTY CLINIC<br/>
+				LABORATORY UNIT<br/>
+			</h3>		
+		</td>
+	  </tr>
+	</table>
+
+<?php
+/*
+    echo "<b>MARIKINA ORTHOPEDIC SPECIALTY CLINIC</b><br/>";
+    echo "<b>OUT-PATIENT ORTHOPEDICS, X-RAY, PHYSICAL AND OCCUPATION THERAPY</b><br/>";
+    echo "<b>#2 E. MANALO AVE., STO. NIÑO, 1820, MARIKINA CITY, PHILIPPINES</b><br/>";
+    echo "<b>TEL. NO. (8) 941-4888; MONDAY - SATURDAY; 9:00 A.M. -  5:00 P.M.</b><br/>";
+*/
+
+	echo "<table>";
+/*
+	echo "<tr>";
+	echo "<td>";
+*/					
+	//TO-DO: -add: notes column
+
+	//table headers
+?>	
+<!--
+	<tr>
+	<td class='tableHeaderColumn'><b>COUNT</b></td>
+	<td class='tableHeaderColumn'><b>BODY LOCATION</b></td>
+	<td class='tableHeaderColumn'><b>TYPE</b></td>
+	<td class='tableHeaderColumn'><b>PRICE</b></td>
+	<td class='tableHeaderColumn'><b>SC/PWD<br/>PRICE</b></td>
+	<tr>
+-->
+<?php
+echo "<br/>";
+	echo "<table>";
+				
+	//TO-DO: -add: auto-identify and update date format to use YYYY-MM-DD
+	
+	echo '<tr class="row">';
+
+	ini_set('auto_detect_line_endings', true);
+
+	//added by Mike, 20200523
+	if (!file_exists($filename)) {
+		//add the day of the week
+		//edited by Mike, 20200726
+		//$sDateToday = (new DateTime())->format('Y-m-d, l');
+		$sDateToday = Date('Y-m-d, l');
+
+		echo $filename;
+
+		echo "There are no transactions for the day, ".$sDateToday.".";
+	}
+	else {
+		//Reference: https://stackoverflow.com/questions/9139202/how-to-parse-a-csv-file-using-php;
+		//answer by: thenetimp, 20120204T0730
+		//edited by: thenetimp, 20170823T1704
+
+		$iRowCount = -1; //we later add 1 to make start value zero (0)
+		//if (($handle = fopen("test.csv", "r")) !== FALSE) {
+		if (($handle = fopen($filename, "r")) !== FALSE) {
+		  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+			$num = count($data) -1; //we add -1 for the computer to not include the excess cell due to the ending \n
+		//    echo "<p> $num fields in line $row: <br /></p>\n";
+			$iRowCount++;
+			for ($iColumnCount=0; $iColumnCount <= $num; $iColumnCount++) {
+		//        echo $data[$c] . "<br />\n";
+				//edited by Mike, 20200725
+				//echo "<td class='column'>".utf8_encode($data[$iColumnCount])."</td>";
+				
+				//added by Mike, 20200726
+				//$cellValue = $data[$iColumnCount];	
+				$cellValue = utf8_encode($data[$iColumnCount]);
+				
+				if (is_numeric($cellValue)) {
+					if ((strpos($cellValue,"#")!==false)) {
+						//integer value
+					}						
+					else {
+						//add two digits after the decimal point
+						//Reference: https://www.php.net/number_format;
+						//last accessed: 20200812
+						//input: 60
+						//output: 60.00
+						//Note: Rounding Rules
+						//input: 60.00000000000006
+						//output: 60.00
+						//input: 60.005
+						//output: 60.01
+						//input: 60.004
+						//output: 60.00
+						$cellValue = number_format($cellValue, 2, '.', ',');
+					}
+					echo "<td class='column' style='text-align:right'><b>".$cellValue."</b></td>";
+				}
+				else {
+					if ($iColumnCount==0) {
+						echo "<td class='column' style='text-align:left'>".$cellValue."</td>";
+					}
+					else {
+						echo "<td class='column'>".$cellValue."</td>";
+					}
+				}
+			}
+			echo '</tr><tr class="row">';
+		  }
+		  echo '</tr>';
+		  
+		  fclose($handle);
+		}
+	}
+?>
+	</table>
+	<br />
+	<div>***NOTHING FOLLOWS***</div>
+	<br />
+	<br />
+	<div class="copyright">
+		<span>© Usbong Social Systems, Inc. 2011~2020. All rights reserved.</span>
+	</div>		 
+  </body>
+</html>
