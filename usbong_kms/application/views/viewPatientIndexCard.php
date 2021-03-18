177b51468bfc5ec92bbc4f2036b5fcd434d57a0e
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-03-18 08:20:27 +0800

    Add files via upload

--- usbong_kms/application/views/viewPatientIndexCard.php
+++ usbong_kms/application/views/viewPatientIndexCard.php
@@ -5,7 +5,7 @@
   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
   @author: Michael Syson
   @date created: 20200818
-  @date updated: 20210316
+  @date updated: 20210318
 
   Input:
   1) Laboratory Request Form (.csv format) at the Marikina Orthopedic Specialty Clinic (MOSC)
