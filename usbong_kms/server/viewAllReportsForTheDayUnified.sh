436e76b9ee109fb8f1d08a70300940b9febfbbd7
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-03-21 15:07:56 +0800

    Add files via upload

---
+++ usbong_kms/server/viewAllReportsForTheDayUnified.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# @echo off
+#
+#  Copyright 2020~2021 Usbong Social Systems, Inc.
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+#  @author: Michael Syson
+#  @date created: 2020
+#  @date updated: 20210321
+#
+
+# TO-DO: -update: computer server web address location
+# TO-DO: -add: auto-set computer server web address location
+  
+# edited by Mike, 20210321
+# "C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewReportMedicineUnified" "http://localhost/usbong_kms/index.php/report/viewReportMedicineAsteriskUnified" "http://localhost/usbong_kms/index.php/report/viewReportNonMedicine" "http://localhost/usbong_kms/index.php/report/viewReportSnackUnified" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Pedro" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Peter" "http://localhost/usbong_kms/server/getSalesReportsForTheDay.php" "http://localhost/usbong_kms/index.php/report/viewReceiptReportForTheDay" "http://localhost/usbong_kms/index.php/report/viewReceiptReportPASForTheDay" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueueAccounting"
+
+firefox "http://localhost/usbong_kms/index.php/report/viewReportMedicineUnified" "http://localhost/usbong_kms/index.php/report/viewReportMedicineAsteriskUnified" "http://localhost/usbong_kms/index.php/report/viewReportNonMedicine" "http://localhost/usbong_kms/index.php/report/viewReportSnackUnified" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Pedro" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Peter" "http://localhost/usbong_kms/server/getSalesReportsForTheDay.php" "http://localhost/usbong_kms/index.php/report/viewReceiptReportForTheDay" "http://localhost/usbong_kms/index.php/report/viewReceiptReportPASForTheDay" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueueAccounting"&
