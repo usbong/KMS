f248cf009f879b105fd015c080d41943b6c7f7fe
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-03-22 17:27:04 +0800

    Delete autoScreenCapturerReceiptReportForTheMonthMOSC.bat

--- usbong_kms/kasangkapan/autoScreenCapturerReceiptReportForTheMonthMOSC.bat
+++
@@ -1,5 +0,0 @@
-phantomjs saveWebPageAsImageFile.js viewReceiptReport
-
-set myDate=%date:~10,4%%date:~4,2%%date:~7,2%
-
-explorer "C:\xampp\htdocs\usbong_kms\kasangkapan\phantomjs-2.1.1-windows\bin\output\"%myDate%
