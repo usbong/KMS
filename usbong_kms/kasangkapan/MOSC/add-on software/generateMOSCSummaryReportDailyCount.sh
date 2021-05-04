b5e9c449234d459b072e01d99b82163397dd4677
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-05-04 13:08:18 +0800

    Add files via upload

---
+++ usbong_kms/kasangkapan/MOSC/add-on software/generateMOSCSummaryReportDailyCount.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#TO-DO: -update: this
+
+#
+#	Auto-generate MOSC Summary Report Daily Count
+# Copyright 2020~2021 Usbong Social Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+#
+# @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
+# @author: SYSON, MICHAEL B.
+# @date created: 20200306
+# @last updated: 20210504
+#
+
+#cd /d %1%
+#set mainDirectory=%CD%
+#cd /d %mainDirectory%
+
+set mainDirectory="./"
+#cd /d $mainDirectory
+cd $mainDirectory
+
+#TO-DO: update: this
+#cd assets/transactions/
+#mkdir tempListBeforeProcessing
+#copy *List.txt "tempListBeforeProcessing/"
+#xcopy "tempListBeforeProcessing\*List.txt" "." /s /y
+
+#cd %mainDirectory%
+#cd $mainDirectory
+
+#Windows Machine
+#java -cp .\software:.\software\org.apache.commons.text.jar generateMOSCSummaryReportDailyCount input/cashier/*.txt assets/*.txt
+
+java -cp ./software:./software/org.apache.commons.text.jar generateMOSCSummaryReportDailyCount input/cashier/*.txt assets/*.txt
+
+#TO-DO: update: this
+cd assets/transactions/
+del *List.txt
+
+rename *ListTemp.txt *List.txt
+
+#cd %mainDirectory%
+cd $mainDirectory
+cd ..
+
+# TO-DO: -update: this
+# "add-on software"\requi#ents\"chrome.exe - Shortcut.lnk" file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputTreatment.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOfUnclassifiedDiagnosedCasesOutput.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputConsultation.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsConsultation.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsProcedure.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsTreatment.html
+
+#PAUSE
