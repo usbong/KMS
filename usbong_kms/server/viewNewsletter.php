bd61eae403d96f842719e6c714d07a447939dc2a
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-07-01 08:05:13 +0800

    Add files via upload

---
+++ usbong_kms/server/viewNewsletter.php
@@ -0,0 +1,262 @@
+<!--
+  Copyright 2021 SYSON, MICHAEL B.
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+
+  @company: USBONG
+  @author: SYSON, MICHAEL B.
+  @date created: 20210701
+  @date updated: 20210701
+  @website address: http://www.usbong.ph
+
+  Input:
+  1) MySQL Database with Newsletter details
+
+  Output:
+  1) Auto-generated print-ready Newsletter that is viewable on a Computer Web Browser  
+  
+  Computer Web Browser Address (Example):
+  1) http://localhost/usbong_kms/server/viewNewsletter.php   
+-->
+<?php
+//defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+   <!-- <meta charset="utf-8"> -->
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <!-- Reference: Apache Friends Dashboard index.html -->
+    <!-- "Always force latest IE rendering engine or request Chrome Frame" -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+    <style type="text/css">
+	/**/
+	                    body
+                        {
+							font-family: Arial;
+							font-size: 12pt;
+
+							/* This makes the width of the output page that is displayed on a browser equal with that of the printed page. */
+							/* Legal Size; Landscape*/							
+
+							width: 670px;/*860px;*//* 802px;*//* 670px */
+
+							/* removed by Mike, 20210105 */
+							/* use zoom 67% scale*/
+							/* at present, command not support in Mozilla Firefox */				
+/*							zoom: 67%; 
+	
+							transform: scale(0.67);
+							transform-origin: 0 0;	
+*/							
+                        }
+						
+						div.copyright
+						{
+							text-align: center;
+						}
+						
+						div.formTitle
+						{
+							text-align: center;
+/*							font-weight: bold;*/	
+							font-size: 14pt;
+						}
+						
+						div.newsletterTitle
+						{
+							text-align: center;
+							font-size: 12pt;
+						}
+						
+						img.Image-companyLogo {
+							max-width: 60%;
+							height: auto;
+							float: left;
+							text-align: center;
+							padding-left: 20px;
+							padding-top: 10px;
+						}
+
+						img.Image-moscLogo {
+							max-width: 20%;
+							height: auto;
+							float: left;
+							text-align: center;
+						}
+						
+						table.imageTable
+						{
+							width: 100%;
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}						
+
+						table.tablePart2
+						{
+							width: 100%;
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}						
+
+						table.tablePart3
+						{
+							width: 100%;
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}						
+
+						tr.rowEvenNumber {
+							background-color: #dddddd; <!--#dddddd; = gray #95b3d7; = sky blue; use as row background color-->
+							border: 1pt solid #00ff00;		
+						}
+
+						td.tableHeaderColumnPart1
+						{
+							text-align: left;
+/*							font-weight: bold;
+*/
+						}						
+
+						td.tableHeaderColumnPart2
+						{
+							border: 1px solid #000000;		
+							text-align: center;
+/*							font-weight: bold;
+*/
+						}						
+
+						td.tableHeaderColumnPart3dot1
+						{
+							text-align: left;
+/*							font-weight: bold;
+*/
+							width: 70%;							
+						}						
+
+						td.tableHeaderColumnPart3dot2
+						{
+							text-align: left;
+/*							font-weight: bold;
+*/
+							width: 30%;							
+						}						
+
+
+						td.column
+						{
+							border: 1px dotted #000000;		
+							text-align: center;
+						}						
+
+						td.columnFee
+						{
+							border: 1px dotted #000000;		
+							text-align: right;
+						}
+
+						td.columnFeeTotal
+						{
+							border: 1px dotted #000000;		
+							text-align: right;
+							font-size: 14pt;
+							font-weight: bold;
+						}
+						
+
+						td.columnBorderBottom
+						{
+							border: 1px dotted #ab9c7d;		
+							border-bottom: 4px double black;
+							text-align: center;
+						}						
+
+						td.columnBorderBottomDotted
+						{
+							border: 1px dotted #ab9c7d;		
+							border-bottom: 2px dotted black;
+							text-align: center;
+						}						
+
+						td.columnBorderTopBottom
+						{
+							border: 1px dotted #ab9c7d;		
+							border-top: 2px solid black;
+							border-bottom: 4px double black;
+							text-align: center;
+						}						
+						
+						td.imageColumn
+						{
+							width: 40%;
+							display: inline-block;
+						}						
+
+						td.pageNameColumn
+						{
+							width: 50%;
+							display: inline-block;
+							text-align: right;
+						}												
+    /**/
+    </style>
+    <title>
+      USBONG Newsletter
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style type="text/css">
+    </style>
+  </head>
+	  <script>
+	  </script>
+  <body>
+<?php
+	date_default_timezone_set('Asia/Hong_Kong');
+	
+	//edited by Mike, 20200726
+	//$dateToday = (new DateTime())->format('Y-m-d');
+	$dateToday = Date('Y-m-d');
+
+	// connect to the database
+	include('usbong-kms-connect.php');
+
+	$mysqli->set_charset("utf8");
+
+	//TO-DO: -update: this
+
+?>
+	<div class="formTitle">
+		<b>USBONG NEWSLETTER</b>
+	</div>
+
+<!--	<b>DATE:</b><?php echo " ".$dateToday; ?>
+-->
+	<br>	
+	<div class="newsletterTitle">
+		EXPENSES > INCOME PATTERN<br/>
+		Manloloko as 化け物 (bakemono; naibang bagay)<br/>
+		Solution: Alles klar (All clear)<br/>
+	</div>
+	<br>	
+	JULY  2021
+	<br>
+	<br>
+
+PHILIPPINES: USBONG CONTINUES TO PROVIDE
+COMPUTER SERVICE ASSISTANCE
+BY SHARING KNOW-HOW ON MAPARAANG PAGBIBILANG
+	
+	
+	<br>
+	<br>
+	<br />
+	<div class="copyright">
+		<span>© SYSON, MICHAEL B. & USBONG. 2011~2021. All rights reserved.</span>
+	</div>		 
+  </body>
+</html>
