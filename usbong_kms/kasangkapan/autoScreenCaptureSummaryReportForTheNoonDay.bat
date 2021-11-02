bec6557751765493568488e83b849efe9578ad2f
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-11-02 16:32:03 +0800

    Add files via upload

--- usbong_kms/kasangkapan/autoScreenCaptureSummaryReportForTheNoonDay.bat
+++ usbong_kms/kasangkapan/autoScreenCaptureSummaryReportForTheNoonDay.bat
@@ -1,4 +1,6 @@
-@echo off
+@ECHO OFF
+REM
+REM Copyright 2020~2021 SYSON, MICHAEL B.
 REM
 REM Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
 REM
@@ -6,9 +8,10 @@ REM http://www.apache.org/licenses/LICENSE-2.0
 REM
 REM Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
 REM
-REM @author: Michael Syson
+REM @company: USBONG
+REM @author: SYSON, MICHAEL B.
 REM @date created: 20201016
-REM @date updated: 20201028
+REM @date updated: 20211102
 REM
 REM Reference:
 REM 1) https://phantomjs.org/; last accessed: 20200724
@@ -25,6 +28,11 @@ REM edited by Mike, 20201018
 REM phantomjs saveWebPageAsImageFile.js viewSummaryReportForTheDay -s -noon
 phantomjs saveWebPageAsImageFile.js viewSummaryReportForTheNoonDay -s -noon
 phantomjs saveWebPageAsImageFile.js viewReportPatientQueueAccounting -n -noon
+
+REM added by Mike, 20211102
+phantomjs saveWebPageAsImageFile.js viewReceiptReportForTheDay
+phantomjs saveWebPageAsImageFile.js viewReceiptReportPASForTheDay
+
 
 set myDate=%date:~10,4%%date:~4,2%%date:~7,2%
 
