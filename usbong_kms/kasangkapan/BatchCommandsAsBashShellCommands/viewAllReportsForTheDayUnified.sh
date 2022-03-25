0fa1c3f14481b9d5afdba2d18c8c4e15d7a7801f
Masarap Mabuhay <masarapmabuhay@gmail.com>
2022-03-25 13:38:41 +0800

    Add files via upload

---
+++ usbong_kms/kasangkapan/BatchCommandsAsBashShellCommands/viewAllReportsForTheDayUnified.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# @echo off
+#
+#  Copyright 2020~2022 SYSON, MICHAEL B.
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+#
+#  @company: USBONG
+#  @author: SYSON, MICHAEL B.
+#  @date created: 2020
+#  @date updated: 20220325
+#  @website address: http://www.usbong.ph
+#
+
+#edited by Mike, 20220325
+#"C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewReportMedicineUnified" "http://localhost/usbong_kms/index.php/report/viewReportMedicineAsteriskUnified" "http://localhost/usbong_kms/index.php/report/viewReportNonMedicine" "http://localhost/usbong_kms/index.php/report/viewReportSnackUnified" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Pedro" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Peter" "http://localhost/usbong_kms/server/getSalesReportsForTheDay.php" "http://localhost/usbong_kms/index.php/report/viewReceiptReportForTheDay" "http://localhost/usbong_kms/index.php/report/viewReceiptReportPASForTheDay" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueue" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueueAccounting"
+
+firefox --new-window "http://localhost/usbong_kms/index.php/report/viewReportMedicineUnified" "http://localhost/usbong_kms/index.php/report/viewReportMedicineAsteriskUnified" "http://localhost/usbong_kms/index.php/report/viewReportNonMedicine" "http://localhost/usbong_kms/index.php/report/viewReportSnackUnified" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Pedro" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Peter" "http://localhost/usbong_kms/server/getSalesReportsForTheDay.php" "http://localhost/usbong_kms/index.php/report/viewReceiptReportForTheDay" "http://localhost/usbong_kms/index.php/report/viewReceiptReportPASForTheDay" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueue" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueueAccounting"
