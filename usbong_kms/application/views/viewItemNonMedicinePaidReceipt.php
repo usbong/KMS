c0e0380579e8e7c1dcf67345b77e6e25e6ed5acc
Masarap Mabuhay <masarapmabuhay@gmail.com>
2022-07-12 09:24:35 +0800

    Add files via upload

--- usbong_kms/application/views/viewItemNonMedicinePaidReceipt.php
+++ usbong_kms/application/views/viewItemNonMedicinePaidReceipt.php
@@ -1,5 +1,5 @@
 <!--
-' Copyright 2020~2021 SYSON, MICHAEL B.
+' Copyright 2020~2022 SYSON, MICHAEL B.
 '
 ' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
 '
@@ -10,7 +10,7 @@
 ' @company: USBONG
 ' @author: SYSON, MICHAEL B.
 ' @date created: 20200306
-' @date updated: 20210928
+' @date updated: 20220712; from 20210928
 -->
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
@@ -838,7 +838,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								</div>								
 							</td>
 							<td class ="columnName">				
-								<a href='<?php echo site_url('browse/viewPatient/'.$value['patient_id'])?>' id="viewPatientId<?php echo $iCount?>">
+								<a target='_blank' href='<?php echo site_url('browse/viewPatient/'.$value['patient_id'])?>' id="viewPatientId<?php echo $iCount?>">
 									<div class="patientName">
 					<?php
 									echo $value['patient_name'];
@@ -847,7 +847,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								</a>
 							</td>							
 							<td class ="column">				
-								<a href='<?php echo site_url('browse/viewItemNonMedicine/'.$value['item_id'])?>' id="viewItemId<?php echo $iCount?>">
+								<a target='_blank' href='<?php echo site_url('browse/viewItemNonMedicine/'.$value['item_id'])?>' id="viewItemId<?php echo $iCount?>">
 									<div class="itemName">
 					<?php
 									echo $value['item_name'];
