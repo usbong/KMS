77626e09c7a27904870dca5c4a86c2da7329f89f
Masarap Mabuhay <masarapmabuhay@gmail.com>
2022-07-15 08:19:58 +0800

    Add files via upload

---
+++ usbong_kms/server/videos/viewKMS.php
@@ -0,0 +1,544 @@
+<!--
+  Copyright 2021~2022 SYSON, MICHAEL B.
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+  
+  @company: USBONG
+  @author: SYSON, MICHAEL B.
+  @date created: 20220102
+  @date updated: 20220715; from 20220615
+  @website address: http://www.usbong.ph
+    
+  Input:
+  1) Video File
+  
+  Output:
+  1) Video file that is viewable on a Computer Web Browser  
+  
+  Computer Browser: Website Address (Example):
+  1) local: http://localhost/usbong_newsletters/server/videos/viewKMS.php  
+  2) public internet: http://store.usbong.ph/server/videos/viewKMS.php
+  
+  Recommended Reference:
+  1) https://www.w3schools.com/howto/howto_css_table_center.asp;
+  //last accessed: 20210703
+-->
+<?php
+//defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+   <!-- <meta charset="utf-8"> -->
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <!-- Reference: Apache Friends Dashboard index.html -->
+    <!-- "Always force latest IE rendering engine or request Chrome Frame" -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+    <style type="text/css">
+	/**/
+	                    body
+                        {
+							font-family: Arial;
+							font-size: 12pt;
+							color: rgb(120,63,4);
+							font-weight: bold;
+
+							/* This makes the width of the output page that is displayed on a browser equal with that of the printed page. */
+							/* Legal Size; Landscape*/							
+
+							width: 670px;/*860px;*//* 802px;*//* 670px */
+
+							/* removed by Mike, 20210105 */
+							/* use zoom 67% scale*/
+							/* at present, command not support in Mozilla Firefox */				
+/*							zoom: 67%; 
+	
+							transform: scale(0.67);
+							transform-origin: 0 0;	
+*/							
+                        }
+												
+						div.formTitle
+						{
+							text-align: right;
+/*							font-weight: bold;*/	
+							font-size: 12pt;
+							color: rgb(0,0,0); /* black */
+						}
+						
+						div.newsletterTitle
+						{
+							text-align: center;
+							font-size: 16pt;
+							font-weight: bold;
+						}
+
+						div.newsletterSubTitle
+						{
+							text-align: center;
+							font-size: 12pt;
+							color: #222222; /* rgb(0,0,0); //black */
+							font-weight: bold;
+						}
+						
+						div.newsletterDate
+						{
+							text-align: right;
+							font-size: 12pt;
+							font-weight: bold;
+							color: #cd580f /* #e69138 orange */
+						}
+
+						div.copyright
+						{
+							text-align: center;
+							font-size: 10pt;
+							color: rgb(0,0,0); /* black */
+						}
+					
+						h3
+						{
+							color: rgb(0,0,0); /* black */							
+						}
+					
+						h4
+						{
+							color: rgb(64,64,64);	
+						}
+
+						h5
+						{							
+							color: rgb(85,43,0);	
+						}
+						
+						b
+						{
+							color: rgb(0,0,0); /* from black to white */ 							
+						}
+
+						img.Image-companyLogo {
+							max-width: 60%;
+							height: auto;
+							float: left;
+							text-align: center;
+							padding-left: 20px;
+							padding-top: 10px;
+						}
+
+						img.Image-moscLogo {
+							max-width: 20%;
+							height: auto;
+							float: left;
+							text-align: center;
+						}
+						
+						img.newsletterImage {
+							max-width: 100%;
+							height: auto;
+							border: 1px solid #ab9c7d;
+						}
+
+						img.noteImage {
+							max-width: 100%;
+							height: auto;
+							border: 1px solid #ab9c7d;
+						}
+						
+						div.newsletterImageTextDiv {
+							text-align: center;							
+							font-weight: bold;
+						}
+						
+						table.imageTable
+						{
+							width: 100%;
+						}						
+
+						td.imageTableColumn
+						{
+							border: 1px solid #ab9c7d;
+						}
+
+						table.noteTaxTable
+						{
+							width: 86%;
+							border: 1px solid #ab9c7d;
+							border-collapse: collapse;							 
+							font-size: 10pt;
+							/* Commands to Center-Align Table */
+							margin-left: auto;
+							margin-right: auto;
+						}
+						
+						td.noteTaxTableColumnHeader
+						{
+							border: 1px solid #ab9c7d;
+							background-color: #dddddd;
+							text-align: center;
+						}
+
+						td.noteTaxTableColumn
+						{
+							border: 1px solid #ab9c7d;
+							text-align: left;
+						}
+
+						table.noteTable
+						{
+							border: 1px solid #ab9c7d;
+							width: 90%;
+							margin: 0px;
+							padding: 0px;
+						}
+
+						td.noteTableImageColumn
+						{
+							width:10%;
+							vertical-align: top;
+						}
+				
+						td.noteTableTextColumn
+						{
+							width:100%;
+							text-align: left;							
+							float: left;
+							margin:0;
+							padding:0;
+							display: inline-block;
+							font-weight: bold;							
+							font-size: 11pt;
+							border: 1px solid #ab9c7d;							
+						}
+				
+						table.tablePart2
+						{
+							width: 100%;
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}						
+
+						table.tablePart3
+						{
+							width: 100%;
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}	
+
+						a.webServiceLink
+						{
+							color: rgb(51,145,23);
+							font-weight: bold;							
+						}
+						
+						a.menuLink
+						{
+							color: rgb(64,64,64);
+							text-decoration: none;
+						}
+
+						a.menuLink:hover
+						{
+							color: rgb(64,64,64);
+							text-decoration: underline;
+						}
+
+						table.menuTable
+						{
+							width: 80%;
+							border: 1px solid rgb(146,208,80);
+							border-collapse: collapse;							 
+							font-size: 10pt;
+							/* Commands to Center-Align Table */
+							margin-left: auto;
+							margin-right: auto;
+						}
+
+
+						tr.menuRow
+						{
+							background-color: rgb(146,208,80);
+							text-align: center;							
+							font-weight: 1;
+						}
+						
+						td.menuColumn
+						{
+							background-color: rgb(146,208,80);
+							text-align: center;							
+						}
+
+						td.menuColumn:hover
+						{
+							background-color: rgb(121,176,62);
+							text-align: center;							
+						}
+
+						tr.rowEvenNumber {
+							background-color: #dddddd; <!--#dddddd; = gray #95b3d7; = sky blue; use as row background color-->
+							border: 1pt solid #00ff00;		
+						}
+
+						td.tableHeaderColumnPart1
+						{
+							text-align: left;
+/*							font-weight: bold;
+*/
+						}						
+
+						td.tableHeaderColumnPart2
+						{
+							border: 1px solid #000000;		
+							text-align: center;
+/*							font-weight: bold;
+*/
+						}						
+
+						td.tableHeaderColumnPart3dot1
+						{
+							text-align: left;
+/*							font-weight: bold;
+*/
+							width: 70%;							
+						}						
+
+						td.tableHeaderColumnPart3dot2
+						{
+							text-align: left;
+/*							font-weight: bold;
+*/
+							width: 30%;							
+						}						Newsletter202201
+
+
+						td.column
+						{
+							border: 1px dotted #000000;		
+							text-align: center;
+						}						
+
+						td.columnFee
+						{
+							border: 1px dotted #000000;		
+							text-align: right;
+						}
+
+						td.columnFeeTotal
+						{
+							border: 1px dotted #000000;		
+							text-align: right;
+							font-size: 14pt;
+							font-weight: bold;
+						}
+						
+
+						td.columnBorderBottom
+						{
+							border: 1px dotted #ab9c7d;		
+							border-bottom: 4px double black;
+							text-align: center;
+						}						
+
+						td.columnBorderBottomDotted
+						{
+							border: 1px dotted #ab9c7d;		
+							border-bottom: 2px dotted black;
+							text-align: center;
+						}						
+
+						td.columnBorderTopBottom
+						{
+							border: 1px dotted #ab9c7d;		
+							border-top: 2px solid black;
+							border-bottom: 4px double black;
+							text-align: center;
+						}						
+						
+						td.imageColumn
+						{
+							width: 40%;
+							display: inline-block;
+						}						
+
+						td.pageNameColumn
+						{
+							width: 50%;
+							display: inline-block;
+							text-align: right;
+						}												
+    /**/
+    </style>
+    <title>
+      USBONG Video
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style type="text/css">
+    </style>
+<!-- //edited by Mike, 20220102
+	<base href="http://localhost/usbong_newsletters/" target="_blank">
+-->
+
+	<base href="http://store.usbong.ph/" target="_blank">
+
+  </head>
+	  <script>
+	  </script>
+  <body>
+  
+  
+<?php
+	date_default_timezone_set('Asia/Hong_Kong');
+	
+	//edited by Mike, 20200726
+	//$dateToday = (new DateTime())->format('Y-m-d');
+	$dateToday = Date('Y-m-d');
+
+	// connect to the database
+	//edited by Mike, 20210813
+//	include('usbong-kms-connect.php');
+
+	//Reference: https://stackoverflow.com/questions/16048818/include-php-files-when-they-are-in-different-folders;
+	//answer by: AbsoluteƵERØ, 20130416T2355
+	//edited: 20171021T0640	
+		
+	//echo $_SERVER['DOCUMENT_ROOT']; //example output: C:/xampp/htdocs
+/*	//removed by Mike, 20210831
+	include($_SERVER['DOCUMENT_ROOT'].'/usbong_newsletters/server/usbong-kms-connect.php');
+	$mysqli->set_charset("utf8");
+*/
+
+	//TO-DO: -update: this
+
+?>
+	<table class="imageTable">
+	  <tr>
+		<td class="imageColumn">				
+			<img class="Image-companyLogo" src="assets/images/usbongLogo.png">
+		</td>	
+		<td class="pageNameColumn">
+			<div class="formTitle">
+				<b>USBONG VIDEOS<b>
+			</div>		
+		</td>
+	</table>
+	<br/>
+<!--	<b>DATE:</b><?php echo " ".$dateToday; ?>
+-->
+	
+		<!-- menu bar -->
+	<table class="menuTable">
+	  <tr class="menuRow">
+		<td class="menuColumn">				
+<!--
+			<a class="menuLink" href="https://creativecommons.org/licenses/by/4.0/">		
+-->
+			<a class="menuLink" target="_blank" href="http://www.usbong.ph">		
+				Computer
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" target="_blank" href="https://www.usbong.ph/Training">		
+				Services
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" target="_blank" href="https://www.usbong.ph/team">		
+				Team
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" target="_blank" href="https://www.usbong.ph/team">		
+				Partners
+			</a>
+		</td>	
+		<td class="menuColumn">		
+			<a class="menuLink" target="_blank" href="https://github.com/usbong">		
+				Open Source
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" target="_blank" href="https://www.usbong.ph/excel">		
+				EXCEL
+			</a>
+		</td>	
+		<td class="menuColumn">				
+			<a class="menuLink" target="_blank" href="https://www.usbong.ph/contact">		
+				Contact
+			</a>
+		</td>
+	</table>
+
+	<br/>
+	<a class="webServiceLink" href="https://github.com/usbong/KMS" target="_blank">
+	KNOWLEDGE MANAGEMENT SYSTEM</a>
+	<br/>
+	<br>	
+	<div>
+		<b>DATABASE KNOW-HOW: Example Work Output</b><br/>
+<!--
+		<video width="320" height="240" controls>
+		  <source src="assets/images/newsletter/202108/usbongPagongExecSpeedSDLPlusImage32BitOSWin7IntelSoloCPU1Dot2GHz1GBRAMV20210819T1734.mp4" type="video/mp4">
+		  Your browser does not support the video tag.
+		</video><br/>	
+
+		<video width="320" height="240" controls>
+		<video width="480" height="360" controls>
+-->
+		<video width="416" height="312" controls>
+		  <source src="assets/images/videos/usbongKmsDatabaseSavedWorkReminderDeleteExcessDBFiles20220714T1620.mp4" type="video/mp4">
+		  Your browser does not support the video tag.
+		</video><br/>	
+		<br/>
+		<br/>
+
+		<b><a class="webServiceLink" href="https://www.usbong.ph/excel" target="_blank">COMPUTER SOFTWARE AUTOMATION</a>: Example Work Output</b> <br/>
+
+		<video width="416" height="312" controls>
+		  <source src="assets/images/videos/exampleAnnualReportWithBarChartsUsingMSExcelMacroEtCeteraFinishIn11Secs20220101T1049.mp4" type="video/mp4">
+		  Your browser does not support the video tag.
+		</video><br/>	
+		
+		<video width="416" height="312" controls>
+		  <source src="assets/images/videos/halimbawaSetupSystemForQuickExecution20220713T1018.mp4" type="video/mp4">
+		  Your browser does not support the video tag.
+		</video><br/>			
+
+		<video width="416" height="312" controls>
+		  <source src="assets/images/videos/exampleWeeklyHMOReportWithComputerAutomationAssistance20220302T1459.mp4" type="video/mp4">
+		  Your browser does not support the video tag.
+		</video><br/>	
+
+		<video width="416" height="312" controls>
+		  <source src="assets/images/videos/exampleWeeklyCashCollectionReportWithComputerAutomationAssistanceUsingLibreOfficeCalcMacroBasic20220306T0949.mp4" type="video/mp4">
+		  Your browser does not support the video tag.
+		</video><br/>	
+		
+<br/>
+		<br/>
+
+		<b><a class="webServiceLink" href="https://www.usbong.ph/excel" target="_blank">COMPUTER NETWORK</a>: Example Work Output</b> <br/>
+		<br/>
+		<video width="416" height="312" controls>
+		  <source src="assets/images/videos/exampleRJ45CableHeadCorrectlyBuiltToCauseLEDBlinkingPattern20220225T1523.mp4" type="video/mp4">
+		  Your browser does not support the video tag.
+		</video><br/>	
+	
+		
+	</div>
+	<br>	
+	<br/>
+	
+	<div class="copyright">
+		<span>
+			© SYSON, MICHAEL B. & USBONG. 2011~<?php echo date("Y");?>. All rights reserved.<br/>
+			<a class="webServiceLink" target="_blank" href="https://creativecommons.org/licenses/by/4.0/">
+				Creative Commons Attribution License BY 4.0
+			</a>
+		</span>
+	</div>		 
+  </body>
+</html>
