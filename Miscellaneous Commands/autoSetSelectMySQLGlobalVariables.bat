115aeb55b507761bcdfe36df2c39dba1a1110cfc
SYSON, MICHAEL B <masarapmabuhay@gmail.com>
2023-01-26 10:46:34 +0800

    Add files via upload

--- Miscellaneous Commands/autoSetSelectMySQLGlobalVariables.bat
+++ Miscellaneous Commands/autoSetSelectMySQLGlobalVariables.bat
@@ -1,3 +1,4 @@
+@echo off
 REM ' Copyright 2023 SYSON, MICHAEL B.
 REM '
 REM ' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
@@ -16,8 +17,17 @@ REM '
 REM TO-DO: -add: in task scheduler to auto-execute when OS starts
 
 REM note: update file location
-cd C:\Program Files\Mozilla Firefox\
+REM cd C:\Program Files\Mozilla Firefox\
 
-firefox.exe localhost/usbong_kms//server/autoSetSelectMySQLGlobalVariables.php
+cd C:\xampp\php
+
+REM firefox.exe localhost/usbong_kms//server/autoSetSelectMySQLGlobalVariables.php
+
+REM https://stackoverflow.com/questions/4301975/how-can-i-run-a-php-without-a-web-server;
+REM last accessed: 20230126
+REM answer by: Michal M, 20120825T1153; from 20101129T0836
+
+REM note: remove HTML instructions in autoSetSelectMySQLGlobalVariables.php to remove OUTPUT log text
+php.exe "C:\xampp\htdocs\usbong_kms\server\autoSetSelectMySQLGlobalVariables.php"
 
 pause
