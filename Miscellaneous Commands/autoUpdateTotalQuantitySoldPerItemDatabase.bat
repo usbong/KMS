e893c2eff908912fdb30f9c94ef322ebb476df11
SYSON, MICHAEL B <masarapmabuhay@gmail.com>
2023-04-08 09:29:47 +0800

    Add files via upload

---
+++ Miscellaneous Commands/autoUpdateTotalQuantitySoldPerItemDatabase.bat
@@ -0,0 +1,22 @@
+REM
+REM Copyright 2020~2023 USBONG
+REM 
+REM Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+REM
+REM http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+REM
+REM @company: USBONG
+REM @author: SYSON, MICHAEL B.
+REM @date created: 20201216T09:06
+REM @date updated: 20230408; from 20201216
+REM @website address: http://www.usbong.ph
+REM
+REM Notes:
+REM 1) 3 years (approx) since last updated
+REM 2) reusable technique
+
+"C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/browse/updateTotalQuantitySoldPerItem"
+
+exit
