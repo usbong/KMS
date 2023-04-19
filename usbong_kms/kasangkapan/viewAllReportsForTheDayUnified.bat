55a2ff1faa5cd7562cd3d3f6200370c606d5d27e
SYSON, MICHAEL B <masarapmabuhay@gmail.com>
2023-04-19 15:41:55 +0800

    Add files via upload

--- usbong_kms/kasangkapan/viewAllReportsForTheDayUnified.bat
+++ usbong_kms/kasangkapan/viewAllReportsForTheDayUnified.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 REM
-REM  Copyright 2020~2021 SYSON, MICHAEL B.
+REM  Copyright 2020~2023 USBONG
 REM  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
 REM  http://www.apache.org/licenses/LICENSE-2.0
 REM  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
@@ -8,12 +8,17 @@ REM
 REM  @company: USBONG
 REM  @author: SYSON, MICHAEL B.
 REM  @date created: 2020
-REM  @date updated: 20211215
+REM  @date updated: 20230419; from 20211215
 REM
+REM Note: 1) update: firefox.exe location 
+REM
+REM TO-DO: -add: auto-identify location; has "(x86)"?
   
 REM edited by Mike, 20210129
 REM "C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewReportMedicineUnified" "http://localhost/usbong_kms/index.php/report/viewReportMedicineAsteriskUnified" "http://localhost/usbong_kms/index.php/report/viewReportNonMedicineUnified" "http://localhost/usbong_kms/index.php/report/viewReportSnackUnified" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Pedro" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Peter" "http://localhost/usbong_kms/server/getSalesReportsForTheDay.php" "http://localhost/usbong_kms/index.php/report/viewReceiptReportForTheDay" "http://localhost/usbong_kms/index.php/report/viewReceiptReportPASForTheDay" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueueAccounting"
-
 REM edited by Mike, 20211215
 REM "C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewReportMedicineUnified" "http://localhost/usbong_kms/index.php/report/viewReportMedicineAsteriskUnified" "http://localhost/usbong_kms/index.php/report/viewReportNonMedicine" "http://localhost/usbong_kms/index.php/report/viewReportSnackUnified" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Pedro" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Peter" "http://localhost/usbong_kms/server/getSalesReportsForTheDay.php" "http://localhost/usbong_kms/index.php/report/viewReceiptReportForTheDay" "http://localhost/usbong_kms/index.php/report/viewReceiptReportPASForTheDay" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueueAccounting"
-"C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewReportMedicineUnified" "http://localhost/usbong_kms/index.php/report/viewReportMedicineAsteriskUnified" "http://localhost/usbong_kms/index.php/report/viewReportNonMedicine" "http://localhost/usbong_kms/index.php/report/viewReportSnackUnified" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Pedro" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Peter" "http://localhost/usbong_kms/server/getSalesReportsForTheDay.php" "http://localhost/usbong_kms/index.php/report/viewReceiptReportForTheDay" "http://localhost/usbong_kms/index.php/report/viewReceiptReportPASForTheDay" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueue" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueueAccounting"
+
+REM edited by Mike, 20230419
+REM "C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewReportMedicineUnified" "http://localhost/usbong_kms/index.php/report/viewReportMedicineAsteriskUnified" "http://localhost/usbong_kms/index.php/report/viewReportNonMedicine" "http://localhost/usbong_kms/index.php/report/viewReportSnackUnified" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Pedro" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Peter" "http://localhost/usbong_kms/server/getSalesReportsForTheDay.php" "http://localhost/usbong_kms/index.php/report/viewReceiptReportForTheDay" "http://localhost/usbong_kms/index.php/report/viewReceiptReportPASForTheDay" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueue" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueueAccounting"
+"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewReportMedicineUnified" "http://localhost/usbong_kms/index.php/report/viewReportMedicineAsteriskUnified" "http://localhost/usbong_kms/index.php/report/viewReportNonMedicine" "http://localhost/usbong_kms/index.php/report/viewReportSnackUnified" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Pedro" "http://localhost/usbong_kms/index.php/report/viewPayslipWebFor/Peter" "http://localhost/usbong_kms/server/getSalesReportsForTheDay.php" "http://localhost/usbong_kms/index.php/report/viewReceiptReportForTheDay" "http://localhost/usbong_kms/index.php/report/viewReceiptReportPASForTheDay" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueue" "http://localhost/usbong_kms/index.php/report/viewReportPatientQueueAccounting" 
