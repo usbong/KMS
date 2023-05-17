23026a5611f23820e7cde35d8b93ea2f1492afc8
SYSON, MICHAEL B <masarapmabuhay@gmail.com>
2023-05-17 13:52:17 +0800

    Add files via upload

--- usbong_kms/application/models/Browse_Model.php
+++ usbong_kms/application/models/Browse_Model.php
@@ -5036,7 +5036,11 @@ ice, t1.item_id, t1.item_total_sold, t2.quantity_in_stock, t2.expiration_date');
 //		$this->db->where('t2.transaction_date',date("m/d/Y"));
 		$this->db->where('t2.transaction_date',date("m/d/Y", strtotime($transactionDate)));
 				
-		$query = $this->db->get('patient');
+		//added by Mike, 20230517
+		//TO-DO: -verify: this
+		$this->db->limit(1);
+				
+		$query = $this->db->get('patient');		
 
 		$rowArray = $query->result_array();
 				
