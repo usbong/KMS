20e196f7e2734608146c40fbf6c6ffeed3da25c6
SYSON, MICHAEL B <masarapmabuhay@gmail.com>
2023-10-02 14:02:13 +0800

    Add files via upload

--- usbong_kms/kasangkapan/MOSC/add-on software/generateMOSCSummaryReportDailyCount.bat
+++ usbong_kms/kasangkapan/MOSC/add-on software/generateMOSCSummaryReportDailyCount.bat
@@ -1,7 +1,7 @@
 @echo off
 REM
 REM	Auto-generate MOSC Summary Report Daily Count
-REM Copyright 2020 Usbong Social Systems, Inc.
+REM Copyright 2020~2023 USBONG
 REM
 REM Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
 REM
@@ -9,30 +9,37 @@ REM http://www.apache.org/licenses/LICENSE-2.0
 REM
 REM Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
 REM
-REM @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
+REM @company: USBONG
 REM @author: SYSON, MICHAEL B.
 REM @date created: 20200306
-REM @last updated: 20201025
+REM @last updated: 20231002; from 20201025
+REM @website address: www.usbong.ph
 REM
 
 cd /d %1%
 set mainDirectory=%CD%
 cd /d %mainDirectory%
 cd assets/transactions/
-mkdir tempListBeforeProcessing
-copy *List.txt "tempListBeforeProcessing/"
 
-xcopy "tempListBeforeProcessing\*List.txt" "." /s /y
+mkdir tempListBeforeProcessing
+
+REM edited by Mike, 20231002; added auto-overwrite
+REM copy *List.txt "tempListBeforeProcessing/"
+copy *List.txt "tempListBeforeProcessing/" /y
+
+REM removed by Mike, 20231002
+REM xcopy "tempListBeforeProcessing\*List.txt" "." /s /y
 
 cd %mainDirectory%
 		
 REM java -cp .\software;.\software\org.apache.commons.text.jar generateMOSCSummaryReportDailyCount input/cashier/*.txt assets/*.txt
 java -cp .\software;.\software\org.json.jar;.\software\org.apache.commons.text.jar generateMOSCSummaryReportDailyCount input/cashier/ assets/
+
 cd assets/transactions/
 
-del *List.txt
-
-rename *ListTemp.txt *List.txt
+REM removed by Mike, 20231002
+REM del *List.txt
+REM rename *ListTemp.txt *List.txt
 
 cd %mainDirectory%
 
@@ -42,4 +49,6 @@ REM cd ..
 rem TO-DO: -update: this
 rem "add-on software"\requirements\"chrome.exe - Shortcut.lnk" file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputTreatment.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOfUnclassifiedDiagnosedCasesOutput.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlySummaryReportOutputConsultation.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsConsultation.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsProcedure.html file:///D:/2019/add-on%%20software/generateMonthlySummaryReport/add-on%%20software/output/MonthlyStatisticsTreatment.html
 
-PAUSE
+REM edited by Mike, 202301002
+REM PAUSE
+EXIT
