e24e19699aaa0bbca2a2d347000578874ccd73e8
SYSON, MICHAEL B <masarapmabuhay@gmail.com>
2024-03-02 14:24:01 +0800

    Add files via upload

--- Miscellaneous Commands/setOfCommandsToAutoTransferDatabaseStorageFromWindowsToWindowsServer/accessAndSaveDatabaseStorageInWindowsPCFromWindowsPC.bat
+++ Miscellaneous Commands/setOfCommandsToAutoTransferDatabaseStorageFromWindowsToWindowsServer/accessAndSaveDatabaseStorageInWindowsPCFromWindowsPC.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Copyright 2020~2023 SYSON, MICHAEL B.
+REM Copyright 2020~2024 SYSON, MICHAEL B.
 REM
 REM Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
 REM
@@ -12,22 +12,54 @@ REM
 REM @company: USBONG
 REM @author: SYSON, MICHAEL B.
 REM @date created: 20201001
-REM @last modified: 20230517; from 20230207
-REM @website address: http://www.usbong.ph
+REM @last modified: 20240302; from 20231206
+REM @website address: www.usbong.ph
 REM
 
-REM added by Mike, 20230207
+REM edited by Mike, 20240301
+REM Windows 7
 set myDate=%date:~10,4%%date:~4,2%%date:~7,2%
-set myTime=%time:~0,2%%time:~3,2%
-
-REM echo %myTime%
-REM echo %myDate%
-
 
 REM note: no add the day of previous Month at start day of new Month
 REM echo %date:~10,4%(%date:~4,2%-1)
-set myInputMonth=%date:~10,4%%date:~4,2%
+REM set myInputMonth=%date:~10,4%%date:~4,2%
+set myInputMonth=%date:~6,4%%date:~3,2%
+
 REM echo %myInputMonth%
+
+REM auto-identify date format;
+REM based on location of first forward slash, i.e. /
+REM January 9, 2024, Tuesday
+REM in Windows 7, %date% outputs: Tue 01/09/2024
+REM in Windows 11, 09/01/2024
+
+REM note: local date format; dd/mm/yyyy (default in Windows 11)
+REM may vary depending on the settings;
+REM 09/01/2024
+REM year, %date:~6,4%
+REM month, %date:~3,2%
+REM day, %date:~0,2%
+
+REM set myDate=%date:~6,4%%date:~3,2%%date:~0,2%
+
+REM set myDateSlashLocation=/ 
+set myDateSlashLocation=%date:~2,1%
+
+REM echo %myDateSlashLocation%
+
+REM Windows 11 format
+if %myDateSlashLocation%==/ (
+REM	echo "DITO"	
+	set myDate=%date:~6,4%%date:~3,2%%date:~0,2%
+)
+
+REM note: no add the day of previous Month at start day of new Month
+REM set myInputMonth=%myDate:~4,2% REM Month only
+set myInputMonth=%myDate:~0,6%
+
+REM echo %myDate:~4,2%
+
+echo %myInputMonth%
 
 REM access destination folder
 rem sudo mkdir /mnt/myMOSC-AccountingFolder/
@@ -37,20 +69,28 @@ REM source: from Windows machine, e.g. Windows 7, Service Pack 1
 REM Example:
 REM source="//192.168.11.55/accounting/"
 REM source="//192.168.11.55/information desk/"
-set source="\\MOSC-ACCOUNTING\"information desk"
+REM set source="\\MOSC-ACCOUNTING\"information desk"
+
+REM reminder: File Explorer -> Network -> halimbawa_unit;
+REM connect via username and password
+REM set source="\\ORTHOANDPTUNIT\MOSC\DB"
+set source="\\USBONG-HAL\MOSC\DB\"
 
 REM echo "source: "$source
-echo "source: "%source%
+REM echo "source: "%source%
 
 REM update destination location
 REM destination: to Linux machine, e.g. LUBUNTU 20.04 LTS
 REM destination="D:\MOSC"
 REM destination: to Windows machine, e.g. Windows 7
 REM set destination="D:\MOSC"
-set destination="D:\MOSC\DB"
+REM set destination="D:\MOSC\DB"
+REM edited by Mike, 20240302
+set destination="C:\MOSC\DB\"
+
 
 REM echo "destination: "$destination
-echo "destination: "%destination%
+REM echo "destination: "%destination%
 
 REM Note: add quotation marks, e.g. in $source
 REM TO-DO: add: auto-read from external file password=halimbawaValue
