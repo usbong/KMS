4d11b95c68534b2518f2e97c834822cf5bd99fed
SYSON, MICHAEL B. <masarapmabuhay@gmail.com>
2025-02-13 15:54:55 +0800

    Add files via upload

---
+++ usbong_kms/application/views/searchMedicineBoxesLeft.php
@@ -0,0 +1,498 @@
+<!--
+' Copyright 2020~2025 USBONG
+'
+' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+'
+' http://www.apache.org/licenses/LICENSE-2.0
+'
+' Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+'
+' @company: USBONG
+' @author: SYSON, MICHAEL B.
+' @date created: 20200306
+' @date updated: 20250213; from 20230206
+' @website address: http://www.usbong.ph
+-->
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset="utf-8">
+
+    <!-- Reference: Apache Friends Dashboard index.html -->
+    <!-- "Always force latest IE rendering engine or request Chrome Frame" -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+    <style type="text/css">
+	/**/
+	                    body
+                        {
+							font-family: Arial;
+							font-size: 11pt;
+
+							/* This makes the width of the output page that is displayed on a browser equal with that of the printed page. */
+							width: 670px
+                        }
+						
+						div.checkBox
+						{
+							border: 1.5pt solid black; height: 9pt; width: 9pt;
+							text-align: center;
+							float: left
+						}
+						
+						div.option
+						{
+							padding: 2pt;
+							display: inline-block;
+						}
+						
+						div.copyright
+						{
+							text-align: center;
+						}
+						
+						div.itemName
+						{
+							text-align: left;
+						}
+
+						div.tableHeader
+						{
+							font-weight: bold;
+							text-align: center;
+							background-color: #00ff00; <!--#93d151; lime green-->
+							border: 1pt solid #00ff00;
+						}
+
+						input.browse-input
+						{
+							width: 100%;
+							max-width: 500px;
+														
+							resize: none;
+
+							height: 100%;
+						}	
+
+						img.Image-companyLogo {
+							max-width: 60%;
+							height: auto;
+							float: left;
+							text-align: center;
+							padding-left: 20px;
+							padding-top: 10px;
+						}
+
+						img.Image-moscLogo {
+							max-width: 20%;
+							height: auto;
+							float: left;
+							text-align: center;
+						}
+						
+						span.alertSpan {
+							color: red;
+							font-weight: bold;
+						}
+
+						table.search-result
+						{
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}						
+
+						table.imageTable
+						{
+							width: 100%;
+<!--							border: 1px solid #ab9c7d;		
+-->
+						}						
+
+						td.column
+						{
+							border: 1px dotted #ab9c7d;		
+							text-align: right
+						}						
+
+						td.columnTableHeader
+						{
+							font-weight: bold;
+							background-color: #00ff00; <!--#93d151; lime green-->
+<!--							border: 1pt solid #00ff00; -->
+							border: 1px dotted #ab9c7d;		
+							text-align: center;
+						}						
+						
+						td.imageColumn
+						{
+							width: 40%;
+							display: inline-block;
+						}						
+
+						td.pageNameColumn
+						{
+							width: 50%;
+							display: inline-block;
+							text-align: right;
+						}						
+
+    /**/
+    </style>
+    <title>
+      Search Medicine | Boxes Left
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <style type="text/css">
+    </style>
+  </head>
+	  <script>
+		function copyText(iCount){
+//			alert("hello"+iCount);
+	 
+			//Reference: https://stackoverflow.com/questions/51625169/click-on-text-to-copy-a-link-to-the-clipboard;
+			//last accessed: 20200307
+			//answer by: colxi on 20180801; edited by: Lord Nazo on 20180801	 
+/*	 
+			var holdText = document.getElementById("patientNameId"+iCount).innerText;
+
+			const el = document.createElement('textarea');
+		    el.value = holdText;
+			document.body.appendChild(el);
+			el.select();
+			document.execCommand('copy');
+			document.body.removeChild(el);
+
+			//alert("text: "+holdText);
+*/
+			var sHoldTextPatientName = document.getElementById("patientNameId"+iCount).innerText;
+			var sHoldTextFee = document.getElementById("feeId"+iCount).innerText; //.innerText;
+
+//			alert("sHoldTextPatientName: "+sHoldTextPatientName);
+//			alert("sHoldTextFee: "+sHoldTextFee);
+
+			var sHoldTextTransactionTypeName = document.getElementById("transactionTypeNameId"+iCount).innerText;
+
+			var sTreatmentTypeName = document.getElementById("treatmentTypeNameId"+iCount).innerText;
+
+			var sDiscountAmount = "";
+			var sTotalAmount = "0";
+			
+			if (sHoldTextTransactionTypeName=="CASH") {
+				//alert("CASH!");
+				sTotalAmount = sHoldTextFee;
+			}
+			else if (sHoldTextTransactionTypeName=="SC/PWD") {
+				//note: solve the values of the other variables using one (1) known variable value
+				sTotalAmount = sHoldTextFee
+				sHoldTextFee = -sHoldTextFee/(0.20-1);
+				sDiscountAmount = "" + sHoldTextFee*0.20;
+			}
+			else if (sHoldTextTransactionTypeName=="NC") {
+				sHoldTextFee = "NC";				
+				sTotalAmount = "NC";				
+			}						
+			else { //hmo
+				sHoldTextFee = "HMO";				
+				sTotalAmount = sHoldTextTransactionTypeName.toLowerCase();				
+			}
+			
+			const el = document.createElement('textarea');
+/*		    
+			el.value = sHoldTextPatientName+ "\t" + sHoldTextFee + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + sDiscountAmount + "\t" + sTotalAmount;
+			document.body.appendChild(el);
+*/			
+
+			sTreatmentTypeName = sTreatmentTypeName.toUpperCase();
+			
+			if ((sTreatmentTypeName=="SWT") || (sTreatmentTypeName=="SHOCKWAVE")) {
+				el.value = sHoldTextPatientName + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" +sHoldTextFee + "\t" + "\t" + sDiscountAmount + "\t" + sTotalAmount;
+			}
+			else if (sTreatmentTypeName=="LASER") {
+				el.value = sHoldTextPatientName + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" +sHoldTextFee + "\t" + "\t" + sDiscountAmount + "\t" + "\t" + sTotalAmount;
+			}
+			else if (sTreatmentTypeName=="OT") {
+				el.value = sHoldTextPatientName + "\t" + "\t" + sHoldTextFee + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + sDiscountAmount + "\t" + sTotalAmount;
+			}
+			else if (sTreatmentTypeName=="IN-PT") {
+				el.value = sHoldTextPatientName + "\t" + "\t" + "\t" + "\t" + sHoldTextFee + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + sDiscountAmount + "\t" + sTotalAmount;
+			}
+			else {
+				el.value = sHoldTextPatientName+ "\t" + sHoldTextFee + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + sDiscountAmount + "\t" + sTotalAmount;
+			}
+			
+			document.body.appendChild(el);							
+			el.select();
+			document.execCommand('copy');
+			document.body.removeChild(el);
+
+//			alert("text: "+sHoldTextPatientName + sHoldTextFee);//el.value);
+
+		}
+/*	  
+		  defaultScrollWidth = 0;
+		  
+		  function auto_grow(element) {
+			element.style.height = "5px";
+			element.style.height = (element.scrollHeight*4)+"px";
+
+			if (defaultScrollWidth == 0) {
+				defaultScrollWidth = element.scrollWidth; //i.e. 42% of the width of the full width of the Browser Window
+				alert("defaultScrollWidth: "+defaultScrollWidth);
+			}
+			else if (element.scrollWidth < defaultScrollWidth){
+//				defaultScrollWidth = 100%;
+				defaultScrollWidth = element.scrollWidth;
+//				alert("defaultScrollWidth: "+defaultScrollWidth);
+
+			}
+				
+			element.style.width = defaultScrollWidth; //(element.scrollWidth+element.scrollWidth*0.42)+"px";			
+		  }
+*/
+	  </script>
+  <body>
+	<table class="imageTable">
+	  <tr>
+		<td class="imageColumn">				
+			<img class="Image-moscLogo" src="<?php echo base_url('assets/images/moscLogo.jpg');?>">		
+			<img class="Image-companyLogo" src="<?php echo base_url('assets/images/usbongLogo.png');?>">	
+		</td>
+		<td class="pageNameColumn">
+			<h2>
+				Search Medicine | Box Left
+			</h2>		
+		</td>
+	  </tr>
+	</table>
+	<br/>
+	<!-- Form -->
+	<form id="browse-form" method="post" action="<?php echo site_url('browse/confirmMedicineBoxesLeft')?>">
+		<?php
+			$itemCounter = 1;
+		?>
+<!--		<input type="hidden" name="reportTypeIdParam" value="1" required>
+		<input type="hidden" name="reportTypeNameParam" value="Incident Report" required>
+-->
+
+		<div>
+			<table width="100%">
+<!--
+			  <tr>
+				<td>
+				  <b><span>Pangalan</span></b>
+				</td>
+			  </tr>
+-->
+			  <tr>
+				<td>				
+				  <input type="text" class="browse-input" placeholder="" name="nameParam" required>
+				</td>
+			  </tr>
+			</table>
+		</div>
+		<br />
+		<!-- Buttons -->
+		<button type="submit" class="Button-login">
+			Enter
+		</button>
+	</form>
+
+
+	<br/>
+	<br/>
+	
+<!--	<div id="myText" onclick="copyText(1)">Text you want to copy</div>
+-->	
+	<?php
+	
+		//get only name strings from array 
+		if (isset($result)) {			
+			if ($result!=null) {		
+/*			
+				echo "<b>MEDICAL DOCTOR: </b>".$result[0]["medical_doctor_name"];
+				echo "<br/>";
+				echo "<br/>";
+*/			
+
+				$resultCount = count($result);
+				if ($resultCount==1) {
+					echo '<div>Showing <b>'.count($result).'</b> result found.</div>';
+				}
+				else {
+					echo '<div>Showing <b>'.count($result).'</b> results found.</div>';			
+				}			
+
+				echo "<br/>";
+				echo "<table class='search-result'>";
+				
+				//add: table headers
+?>				
+					  <tr class="row">
+						<td class="columnTableHeader">				
+				<?php
+								echo "ITEM NAME";
+				?>		
+						</td>
+						<td class="columnTableHeader">				
+							<?php
+								echo "AVAILABLE"; //IN-STOCK;
+							?>
+						</td>
+						<td class="columnTableHeader">				
+							<?php
+								echo "BOXES LEFT";
+							?>
+						</td>						
+						<td class="columnTableHeader">				
+							<?php
+								echo "EXPIRATION";
+							?>
+						</td>
+						<td class="columnTableHeader">				
+							<?php
+								echo "PRICE"; //ITEM PRICE;
+							?>
+						</td>
+					  </tr>
+<?php				
+				$iCount = 1;
+
+				foreach ($result as $value) {
+		?>				
+		
+					  <tr class="row">
+						<td class ="column">				
+							<a target='_blank' href='<?php echo site_url('browse/viewItemMedicine/'.$value['item_id'])?>' id="viewItemId<?php echo $iCount?>">
+								<div class="itemName">
+				<?php
+								echo $value['item_name'];
+				?>		
+								</div>								
+							</a>
+						</td>
+						<td class =column>				
+								<div id=quantityInStockId<?php echo $iCount?>>
+							<?php
+								//echo $value['quantity_in_stock'];
+
+								//edited by Mike, 20200408
+								if ($value['quantity_in_stock']<0) {
+									echo 9999;
+								}
+								//added by Mike, 20200614
+								else if ($value['quantity_in_stock']=="") {
+									//edited by Mike, 20200615
+									//echo 9999;
+									echo "0 / 0"; //edited by Mike, 20200703
+								}
+								else {
+									//edited by Mike, 20200417
+//									echo $value['quantity_in_stock'];
+									echo $value['resultQuantityInStockNow']." / ".$value['quantity_in_stock'];										
+								}
+							?>
+								</div>
+						</td>
+						<td class =column>				
+								<div id=quantityInStockId<?php echo $iCount?>>
+							<?php
+								//echo $value['quantity_in_stock'];
+
+								//edited by Mike, 20200408
+								if ($value['quantity_in_stock']<0) {
+									echo 9999;
+								}
+								//added by Mike, 20200614
+								else if ($value['quantity_in_stock']=="") {
+									//edited by Mike, 20200615
+									//echo 9999;
+									echo "0 / 0"; //edited by Mike, 20200703
+								}
+								else {
+									//edited by Mike, 20200417
+//									echo $value['quantity_in_stock'];
+									//edited by Mike, 20250213
+									//echo $value['resultQuantityInStockNow']." / ".$value['quantity_in_stock'];				
+									
+									//echo floor($value['resultQuantityInStockNow']/$quantityPerBox); //." / ".($value['quantity_in_stock']/$quantityPerBox);	
+									echo floor($value['resultQuantityInStockNow']/$quantityPerBox)." / ".floor($value['quantity_in_stock']/$quantityPerBox);
+								}
+							?>
+								</div>
+						</td>						
+						<td class =column>				
+								<div id=expirationId<?php echo $iCount?>>
+							<?php
+								//echo $value['expiration_date'];
+								if ($value['expiration_date']==0) {
+									if ($value['quantity_in_stock']==-1) {
+										echo "UNKNOWN";
+									}
+									//added by Mike, 20200614
+									else if ($value['quantity_in_stock']=="") {
+										echo "UNKNOWN";
+									}
+									else {
+										//edited by Mike, 20200828
+										//echo "NONE";
+										echo '<span class="alertSpan">';										
+										echo "NONE";
+										echo '</span>';
+									}
+								}
+								else {
+									//edited by Mike, 20200504
+									//echo $value['expiration_date'];
+									if ($value['expiration_date'] <= date("Y-m-d")) {
+										echo '<span class="alertSpan">';
+									}
+									else {
+										echo '<span>';
+									}
+									echo $value['expiration_date'];
+									echo '</span>';
+								}
+							?>
+								</div>
+						</td>
+						<td class =column>				
+								<div id=itemPriceId<?php echo $iCount?>>
+							<?php
+								echo $value['item_price'];
+							?>
+								</div>
+						</td>
+					  </tr>
+		<?php				
+					$iCount++;		
+//					echo "<br/>";
+				}				
+
+				echo "</table>";				
+				echo "<br/>";				
+				echo '<div>***NOTHING FOLLOWS***';	
+			}
+			else {					
+
+				echo '<div>';					
+				echo 'Your search <b>- '.$nameParam.' -</b> did not match any of our medicine names.';
+				echo '<br><br>Recommendation:';
+				echo '<br>&#x25CF; Reverify that the spelling is correct.';				
+				echo '</div>';					
+			}			
+		}
+	?>
+	<br />
+	<br />
+	<br />
+	<br />
+	<div class="copyright">
+		<span>© <b>www.usbong.ph</b> 2011~<?php echo date("Y");?>. All rights reserved.</span>
+	</div>		 
+  </body>
+</html>
