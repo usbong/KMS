f32ed6e78266234095cfdbe1aadbd5a5150c7fd9
SYSON, MICHAEL B. <masarapmabuhay@gmail.com>
2025-02-15 15:15:45 +0800

    Add files via upload

--- usbong_kms/kasangkapan/autoScreenCaptureReportMedicineBioStrata.bat
+++ usbong_kms/kasangkapan/autoScreenCaptureReportMedicineBioStrata.bat
@@ -1,6 +1,6 @@
 @echo off
 REM
-REM Copyright 2021 USBONG SOCIAL SYSTEMS, INC. (USBONG)
+REM Copyright 2021-2025 USBONG
 REM 
 REM Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
 REM
@@ -8,10 +8,11 @@ REM http://www.apache.org/licenses/LICENSE-2.0
 REM
 REM Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
 REM
-REM @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
+REM @company: USBONG
 REM @author: SYSON, MICHAEL B.
 REM @date created: 20210225
-REM @date updated: 20210225
+REM @date updated: 20250215; from 20250213
+REM @website address: http://www.usbong.ph
 REM
 REM Reference:
 REM 1) https://phantomjs.org/; last accessed: 20200724
@@ -24,9 +25,22 @@ set myDateDay=%date:~0,3%
 REM removed by Mike, 20210225
 REM echo %myDateDay%
 
-REM edited by Mike, 20210225
-REM phantomjs saveWebPageAsImageFile.js "confirmMedicine" 
-phantomjs saveWebPageAsImageFile.js "confirmMedicine/_postlagaflex" 
-phantomjs saveWebPageAsImageFile.js "confirmMedicine/_postdiclogen" 
+REM set myDateSlashLocation=/ 
+set myDateSlashLocation=%date:~2,1%
 
-explorer "C:\xampp\htdocs\usbong_kms\kasangkapan\phantomjs-2.1.1-windows\bin\output\"%myDate%
+REM echo %myDateSlashLocation%
+
+REM Windows 11 format
+if %myDateSlashLocation%==/ (
+REM	echo "DITO"	
+	set myDate=%date:~6,4%%date:~3,2%%date:~0,2%
+)
+
+REM edited by Mike, 20250215; from 20210225
+REM phantomjs saveWebPageAsImageFile.js "confirmMedicine" 
+phantomjs saveWebPageAsImageFile.js "confirmMedicineBoxesLeft/_postlagaflex" 
+phantomjs saveWebPageAsImageFile.js "confirmMedicineBoxesLeft/_postdiclogen" 
+
+REM edited by Mike, 20230418
+REM explorer "C:\xampp\htdocs\usbong_kms\kasangkapan\phantomjs-2.1.1-windows\bin\output\"%myDate%
+explorer "C:\xampp\htdocs\usbong_kms\kasangkapan\output\"%myDate%
